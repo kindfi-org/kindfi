4c3b0a7699eb1cb6ca851444ca1b48dd006680d1
Roberto Lucas <andler.dev@gmail.com>
2025-01-15 04:14:37 -0600

    fix: env vars

--- apps/web/lib/supabase/client.ts
+++ apps/web/lib/supabase/client.ts
@@ -2,8 +2,6 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export const createClient = () =>
 	createBrowserClient(
-		// biome-ignore lint/style/noNonNullAssertion: <explanation>
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		// biome-ignore lint/style/noNonNullAssertion: <explanation>
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
 	)
