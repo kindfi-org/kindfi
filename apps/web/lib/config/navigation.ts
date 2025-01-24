cb6cff61b0a11658e35202672717de7d28a5e4f6
William Ugalde Gamboa <wugalde19@gmail.com>
2025-01-23 23:47:28 -0600

    chore: run linter

--- apps/web/lib/config/navigation.ts
+++ apps/web/lib/config/navigation.ts
@@ -1,5 +1,5 @@
 interface NavigationItem {
-  id: string
+	id: string
 	title: string
 	href: string
 	description: string
@@ -7,19 +7,19 @@ interface NavigationItem {
 
 const projects: NavigationItem[] = [
 	{
-    id: 'explore-projects-id',
+		id: 'explore-projects-id',
 		title: 'Explore Projects',
 		href: '/projects',
 		description: 'Discover verified social initiatives powered by blockchain',
 	},
 	{
-    id: 'create-project-id',
+		id: 'create-project-id',
 		title: 'Create a Project',
 		href: '/create',
 		description: 'Start your social impact campaign with Web3 technology',
 	},
 	{
-    id: 'featured-projects-id',
+		id: 'featured-projects-id',
 		title: 'Featured Projects',
 		href: '/featured',
 		description: 'Explore the most successful initiatives from our community',
@@ -28,20 +28,20 @@ const projects: NavigationItem[] = [
 
 const resources: NavigationItem[] = [
 	{
-    id: 'learn-web3-id',
+		id: 'learn-web3-id',
 		title: 'Learn Web3',
 		href: '/learn',
 		description:
 			'Access guides and resources to understand blockchain and crypto',
 	},
 	{
-    id: 'community-id',
+		id: 'community-id',
 		title: 'Community',
 		href: '/community',
 		description: 'Join our decentralized and collaborative community',
 	},
 	{
-    id: 'social-impact-id',
+		id: 'social-impact-id',
 		title: 'Social Impact',
 		href: '/social-impact',
 		description: 'Learn about the social impact initiatives and their outcomes',
