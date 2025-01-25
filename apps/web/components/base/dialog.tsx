7935290e64896ff2eb3437e00731f056d2663a18
KevinLatino <latinokevin9@gmail.com>
2025-01-25 16:17:51 -0600

    feat: adding semantic html for dialog

--- apps/web/components/base/dialog.tsx
+++ apps/web/components/base/dialog.tsx
@@ -30,25 +30,31 @@ const DialogOverlay = React.forwardRef<
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 const DialogContent = React.forwardRef<
-	React.ElementRef<typeof DialogPrimitive.Content>,
-	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+	HTMLDialogElement,
+	React.ComponentPropsWithoutRef<'dialog'>
 >(({ className, children, ...props }, ref) => (
 	<DialogPortal>
 		<DialogOverlay />
-		<DialogPrimitive.Content
+		<dialog
 			ref={ref}
+			open
+			aria-labelledby="dialog-title"
+			aria-describedby="dialog-description"
 			className={cn(
-				'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+				'fixed left-[50%] top-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 sm:rounded-lg',
 				className,
 			)}
 			{...props}
 		>
 			{children}
-			<DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+			<DialogPrimitive.Close
+				className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
+				aria-label="Close"
+			>
 				<X className="h-4 w-4" />
 				<span className="sr-only">Close</span>
 			</DialogPrimitive.Close>
-		</DialogPrimitive.Content>
+		</dialog>
 	</DialogPortal>
 ))
 DialogContent.displayName = DialogPrimitive.Content.displayName
@@ -56,8 +62,8 @@ DialogContent.displayName = DialogPrimitive.Content.displayName
 const DialogHeader = ({
 	className,
 	...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-	<div
+}: React.HTMLAttributes<HTMLElement>) => (
+	<header
 		className={cn(
 			'flex flex-col space-y-1.5 text-center sm:text-left',
 			className,
@@ -70,8 +76,8 @@ DialogHeader.displayName = 'DialogHeader'
 const DialogFooter = ({
 	className,
 	...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-	<div
+}: React.HTMLAttributes<HTMLElement>) => (
+	<footer
 		className={cn(
 			'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
 			className,
@@ -85,14 +91,15 @@ const DialogTitle = React.forwardRef<
 	React.ElementRef<typeof DialogPrimitive.Title>,
 	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
 >(({ className, ...props }, ref) => (
-	<DialogPrimitive.Title
-		ref={ref}
-		className={cn(
-			'text-lg font-semibold leading-none tracking-tight',
-			className,
-		)}
-		{...props}
-	/>
+	<DialogPrimitive.Title ref={ref} asChild>
+		<h2
+			className={cn(
+				'text-lg font-semibold leading-none tracking-tight',
+				className,
+			)}
+			{...props}
+		/>
+	</DialogPrimitive.Title>
 ))
 DialogTitle.displayName = DialogPrimitive.Title.displayName
 
@@ -100,11 +107,9 @@ const DialogDescription = React.forwardRef<
 	React.ElementRef<typeof DialogPrimitive.Description>,
 	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
 >(({ className, ...props }, ref) => (
-	<DialogPrimitive.Description
-		ref={ref}
-		className={cn('text-sm text-muted-foreground', className)}
-		{...props}
-	/>
+	<DialogPrimitive.Description ref={ref} asChild>
+		<p className={cn('text-sm text-muted-foreground', className)} {...props} />
+	</DialogPrimitive.Description>
 ))
 DialogDescription.displayName = DialogPrimitive.Description.displayName
 
