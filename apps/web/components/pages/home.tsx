79a94e13846cb033467cb88cef3fcad4e872f3b7
Derian <dmrodriguez2000@gmail.com>
2025-01-26 10:24:14 -0600

    impr(web): implement lazy loading for home sections

--- apps/web/components/pages/home.tsx
+++ apps/web/components/pages/home.tsx
@@ -1,13 +1,14 @@
-import { Hero } from "~/components/sections/home/hero";
-import { UserJourney } from "../sections/home/user-journey";
-import { HighlightedProjects } from "~/components/sections/home/highlighted-projects";
-import { JoinUs } from "../sections/home/join-us";
-import { HowItWorks } from "../sections/home/how-it-works";
-import { NewUserGuide } from "../sections/home/new-user-guide";
-import { PlatformOverview } from "../sections/home/platform-overview";
-import { Community } from "../sections/home/community";
-import { FinalCTA } from "../sections/home/final-cta";
+import dynamic from 'next/dynamic';
 
+const Hero = dynamic(() => import('~/components/sections/home/hero').then((mod) => mod.Hero), { ssr: true });
+const UserJourney = dynamic(() => import('~/components/sections/home/user-journey').then((mod) => mod.UserJourney));
+const HighlightedProjects = dynamic(() => import('~/components/sections/home/highlighted-projects').then((mod) => mod.HighlightedProjects));
+const JoinUs = dynamic(() => import('~/components/sections/home/join-us').then((mod) => mod.JoinUs));
+const HowItWorks = dynamic(() => import('~/components/sections/home/how-it-works').then((mod) => mod.HowItWorks));
+const NewUserGuide = dynamic(() => import('~/components/sections/home/new-user-guide').then((mod) => mod.NewUserGuide));
+const PlatformOverview = dynamic(() => import('~/components/sections/home/platform-overview').then((mod) => mod.PlatformOverview));
+const Community = dynamic(() => import('~/components/sections/home/community').then((mod) => mod.Community));
+const FinalCTA = dynamic(() => import('~/components/sections/home/final-cta').then((mod) => mod.FinalCTA));
 
 export function HomeDashboard() {
 	return (
