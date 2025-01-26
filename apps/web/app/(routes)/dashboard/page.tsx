1f49cfa3378954b10cbeba235161c9632444ae3e
Nnaji Benjamin <60315147+Benjtalkshow@users.noreply.github.com>
2025-01-26 21:29:12 +0100

    Delete apps/web/app/(routes)/dashboard/page.tsx

--- apps/web/app/(routes)/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import AchievementPage from '~/components/pages/achievements'
-import type { PageProps } from '~/lib/types'
-
-export default function HomePage(props: PageProps) {
-	return <AchievementPage />
-}
