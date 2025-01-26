a2b3c42a47c53c6a59a7784cf143ca40f6e84264
Favvie <favourabangwu@gmail.com>
2025-01-26 13:15:46 +0100

    fix: remove redundant aria-label from sign-up button

--- apps/web/components/header-auth.tsx
+++ apps/web/components/header-auth.tsx
@@ -52,11 +52,7 @@ export default async function AuthButton() {
 		<div className="flex items-center gap-4">
 			Hey, {user.email}!
 			<form action={signOutAction}>
-				<Button
-					type="submit"
-					variant={'outline'}
-					aria-label="Sign Out of Account"
-				>
+				<Button type="submit" variant={'outline'}>
 					Sign out
 				</Button>
 			</form>
