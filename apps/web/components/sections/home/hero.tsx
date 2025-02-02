Wardlabz/wardfund

[web] impr: rename, organize and implement lazy loading in home sections (#82)
* feat(web): add sections directory structure and organize home components

* impr(web): implement lazy loading for home sections

* chore(web): resolve requested changes from PR #38

* chore(web): resolve requested changes from PR #38

* chore(web): resolve requested changes from PR #38

* chore(web): resolve requested changes from PR #38

* chore(web): resolve requested changes from PR #38

* chore(web): remove unnecessary files

* fix(web): build errors

---------

Co-authored-by: Roberto Lucas <contact@andler.dev>

apps/web/components/sections/home/hero.tsx
@@ -1,0 +1,112 @@
+'use client';
+
+import { motion, useReducedMotion } from 'framer-motion';
+
+import { useCallback } from 'react';
+import { Badge } from '~/components/base/badge';
+import { Button } from '~/components/base/button';
+import { badgeVariants, staggerChildren } from '~/lib/constants/animations';
+import { categories } from '~/lib/mock-data/mock-hero-section';
+import type { Category } from '~/lib/types/home.types';
+
+export function Hero() {
+  const shouldReduceMotion = useReducedMotion();
+
+  const renderCategory = useCallback((category: Category) => (
+    <motion.div
+      key={category.id}
+      variants={badgeVariants}
+      whileHover="hover"
+      whileTap="tap"
+      className="relative"
+      layout // Add layout prop for smooth reflow
+    >
+      <Badge
+        variant="secondary"
+        className={`px-4 py-2 cursor-pointer transition-all duration-300 ${category.color}`}
+      >
+        <motion.span
+          className="mr-2"
+          animate={shouldReduceMotion ? {} : { rotate: [0, 5, -5, 0] }}
+          transition={{
+            duration: 2,
+            repeat: Number.POSITIVE_INFINITY,
+            ease: 'easeInOut',
+          }}
+        >
+          {category.icon}
+        </motion.span>
+        {category.label}
+      </Badge>
+    </motion.div>
+  ), []);
+
+  return (
+    <section 
+      className="relative z-0 min-h-[80vh] bg-gradient-to-b from-purple-50/50 to-white px-4 py-20"
+      aria-labelledby="hero-title"
+      role="banner"
+    >
+      <div className="container mx-auto max-w-6xl">
+        <div className="text-center">
+          <motion.h2
+            className="text-2xl font-bold text-gray-800 mb-6"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            Revolutionizing Social Impact
+          </motion.h2>
+
+          <motion.h1
+            className="text-4xl md:text-5xl font-bold gradient-text mb-8 py-4"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
+            Support Social Causes Using Web3
+          </motion.h1>
+
+          <motion.p
+            className="text-lg text-gray-700 pt-2 my-6"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+          >
+            Every contribution fuels real-world impact. You can support social
+            causes through crypto donations to escrows and unlock exclusive
+            NFTs. WardFund is driving the adoption of Web3 technology for a more
+            connected and empowered world where everyone can make a difference.
+          </motion.p>
+
+          <motion.div
+            className="flex flex-col sm:flex-row gap-4 justify-center mb-16"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+          >
+            <Button size="lg" className="gradient-btn text-white">
+              Support with Crypto
+            </Button>
+            <Button
+              size="lg"
+              variant="outline"
+              className="gradient-border-btn"
+            >
+              Explore Causes
+            </Button>
+          </motion.div>
+
+          <motion.div
+            className="flex flex-wrap justify-center gap-3 mb-6"
+            variants={staggerChildren}
+            initial="initial"
+            animate="animate"
+          >
+            {categories.map(renderCategory)}
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};

apps/web/components/sections/home/how-it-works.tsx
@@ -1,0 +1,31 @@
+'use client';
+
+import { InvestmentModelCard } from '~/components/shared/investment-model-card';
+import { SectionCaption } from '~/components/shared/section-caption';
+import { models } from '~/lib/mock-data/mock-how-it-works-section';
+
+export function HowItWorks() {
+  return (
+    <section className="w-full px-4 py-20 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl">
+        <SectionCaption
+          title="Secure, Transparent, and Powered by Web3"
+          subtitle="At WardFund, we ensure that every donation or contribution is backed by the security and transparency of a Web3-based Escrow system. Smart contracts guarantee that funds reach their intended destination to create real impact."
+          highlightWords={["Powered by Web3"]}
+        />
+
+        <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          {models.map((model, index) => (
+            <InvestmentModelCard
+              key={model.id}
+              {...model}
+              onLearnMore={() =>
+                console.log(`Learn more about model ${model.variant}`)
+              }
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};

apps/web/components/sections/home/join-us.tsx
@@ -1,0 +1,115 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { ChevronRight } from "lucide-react";
+import { Button } from "~/components/base/button";
+import { SectionCaption } from "~/components/shared/section-caption";
+import { fadeInUpAnimation } from "~/lib/constants/animations";
+import { features } from "~/lib/mock-data/mock-join-us-section";
+
+export function JoinUs() {
+  return (
+    <section className="relative py-24 overflow-hidden">
+      {/* Background */}
+      <div className="absolute inset-0">
+        <div className="absolute inset-0 bg-grid-slate-100/50 [mask-image:linear-gradient(0deg,white,rgba(255,255,255,0.6))]" />
+      </div>
+
+      <div className="relative container mx-auto px-4">
+        {/* Section Caption */}
+        <motion.div
+          {...fadeInUpAnimation}
+        >
+          <SectionCaption
+            title="Join the WardFund Revolution: Collaborate and Transform the World with Web3"
+            subtitle="The Web3 community has the power to give back. Support causes that matter, collaborate on impactful projects, and be part of a revolution leveraging Web3 technology to improve lives. Together, we can create lasting and meaningful change."
+            highlightWords={["Transform the World with Web3", "WardFund"]}
+          />
+        </motion.div>
+
+        {/* Feature Cards */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 mb-16">
+          {features.map((feature, index) => (
+            <motion.div
+              key={feature.id}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+            >
+              <article
+                className="group h-full bg-white rounded-2xl p-8 shadow-sm hover:shadow-md transition-all duration-300"
+                role="article"
+                aria-labelledby={`feature-title-${feature.id}`}
+              >
+                {/* Icon */}
+                {feature.icon}
+
+                {/* Content */}
+                <h3 id={`feature-title-${feature.id}`} className="mt-6 text-xl font-semibold text-gray-900">
+                  {feature.title}
+                </h3>
+                <p className="mt-4 text-gray-600 leading-relaxed">
+                  {feature.description}
+                </p>
+
+                {/* Highlight */}
+                <div className="mt-6 flex items-center text-sm font-medium text-blue-600">
+                  {feature.highlight}
+                  <ChevronRight className="w-4 h-4 ml-1 group-hover:translate-x-1 transition-transform duration-200" />
+                </div>
+              </article>
+            </motion.div>
+          ))}
+        </div>
+
+        {/* Call-to-Action Box */}
+        <motion.aside
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.6, delay: 0.3 }}
+          className="relative bg-white rounded-2xl p-8 lg:p-12 shadow-lg max-w-3xl mx-auto overflow-hidden"
+        >
+          {/* Background Pattern */}
+          <div className="absolute inset-0 bg-gradient-to-r from-blue-50 to-sky-50 opacity-50" />
+
+          <div className="relative">
+            <h4 className="text-xl font-semibold text-center text-gray-900 mb-4">
+              WardFund: One Better World
+            </h4>
+            <p className="text-gray-600 mb-8 leading-relaxed">
+              At WardFund, you’re part of a movement demonstrating that Web3 is
+              reshaping the future of social impact. From saving the planet to
+              empowering communities, every contribution makes a difference. Now
+              is the time to use decentralized power to drive true change. Join
+              us in making a real impact, free from the limitations of
+              traditional systems.
+            </p>
+            <div className="flex flex-col sm:flex-row gap-4 justify-center">
+              <Button
+                size="lg"
+                className="gradient-btn text-white px-8"
+                onClick={() => {
+                  // TODO: Implement join revolution action
+                }}
+              >
+                Join the Revolution
+              </Button>
+              <Button
+                size="lg"
+                variant="outline"
+                className="gradient-border-btn hover:bg-teal-50"
+                onClick={() => {
+                  // TODO: Implement discovery navigation
+                }}
+              >
+                Discover more about WardFund
+              </Button>
+            </div>
+          </div>
+        </motion.aside>
+      </div>
+    </section>
+  );
+};

apps/web/lib/mock-data/mock-community-section.tsx
@@ -1,0 +1,40 @@
+import { Rocket, Shield, TrendingUp, Users } from 'lucide-react';
+import type { Benefit, TestimonialData } from '~/lib/types/home.types';
+
+export const benefits: Benefit[] = [
+  {
+    id: 'community-social-impact',
+    icon: <Users className="w-5 h-5" />,
+    text: 'Community for Social Impact',
+  },
+  {
+    id: 'empowering-crypto-supporters',
+    icon: <TrendingUp className="w-5 h-5" />,
+    text: 'Empowering Crypto Supporters',
+  },
+  {
+    id: 'blockchain-verification',
+    icon: <Shield className="w-5 h-5" />,
+    text: 'Blockchain Verification and Security',
+  },
+  {
+    id: 'accelerating-blockchain-adoption',
+    icon: <Rocket className="w-5 h-5" />,
+    text: 'Accelerating Blockchain Adoption',
+  },
+];
+
+export const testimonialData: TestimonialData = {
+  quote: [
+    "The WardFund community becomes an ambassador for your social cause, ",
+    "taking your impact far beyond what traditional Web2 methods can achieve. ",
+    "Web3 connects and empowers people worldwide, creating a transparent, ",
+    "global, and secure network of support and verification powered by ",
+    "blockchain technology and Trustless work's Escrows. Supporting meaningful ",
+    "causes isn't something you can buy—it's a reward you earn by being part ",
+    "of a movement dedicated to real change."
+  ].join(""),
+  author: 'WardFund',
+  role: 'Social Impact Platform',
+  imageUrl: '/placeholder-image.jpg', // Replace with actual image
+};

kindfi-org/kindfi

apps/web/components/sections/home/hero.tsx
@@ -1,0 +1,112 @@
+'use client';
+
+import { motion, useReducedMotion } from 'framer-motion';
+
+import { useCallback } from 'react';
+import { Badge } from '~/components/base/badge';
+import { Button } from '~/components/base/button';
+import { badgeVariants, staggerChildren } from '~/lib/constants/animations';
+import { categories } from '~/lib/mock-data/mock-hero-section';
+import type { Category } from '~/lib/types/home.types';
+
+export function Hero() {
+  const shouldReduceMotion = useReducedMotion();
+
+  const renderCategory = useCallback((category: Category) => (
+    <motion.div
+      key={category.id}
+      variants={badgeVariants}
+      whileHover="hover"
+      whileTap="tap"
+      className="relative"
+      layout // Add layout prop for smooth reflow
+    >
+      <Badge
+        variant="secondary"
+        className={`px-4 py-2 cursor-pointer transition-all duration-300 ${category.color}`}
+      >
+        <motion.span
+          className="mr-2"
+          animate={shouldReduceMotion ? {} : { rotate: [0, 5, -5, 0] }}
+          transition={{
+            duration: 2,
+            repeat: Number.POSITIVE_INFINITY,
+            ease: 'easeInOut',
+          }}
+        >
+          {category.icon}
+        </motion.span>
+        {category.label}
+      </Badge>
+    </motion.div>
+  ), []);
+
+  return (
+    <section 
+      className="relative z-0 min-h-[80vh] bg-gradient-to-b from-purple-50/50 to-white px-4 py-20"
+      aria-labelledby="hero-title"
+      role="banner"
+    >
+      <div className="container mx-auto max-w-6xl">
+        <div className="text-center">
+          <motion.h2
+            className="text-2xl font-bold text-gray-800 mb-6"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            Revolutionizing Social Impact
+          </motion.h2>
+
+          <motion.h1
+            className="text-4xl md:text-5xl font-bold gradient-text mb-8 py-4"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
+            Support Social Causes Using Web3
+          </motion.h1>
+
+          <motion.p
+            className="text-lg text-gray-700 pt-2 my-6"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+          >
+            Every contribution fuels real-world impact. You can support social
+            causes through crypto donations to escrows and unlock exclusive
+            NFTs. KindFi is driving the adoption of Web3 technology for a more
+            connected and empowered world where everyone can make a difference.
+          </motion.p>
+
+          <motion.div
+            className="flex flex-col sm:flex-row gap-4 justify-center mb-16"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+          >
+            <Button size="lg" className="gradient-btn text-white">
+              Support with Crypto
+            </Button>
+            <Button
+              size="lg"
+              variant="outline"
+              className="gradient-border-btn"
+            >
+              Explore Causes
+            </Button>
+          </motion.div>
+
+          <motion.div
+            className="flex flex-wrap justify-center gap-3 mb-6"
+            variants={staggerChildren}
+            initial="initial"
+            animate="animate"
+          >
+            {categories.map(renderCategory)}
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};

apps/web/components/sections/home/how-it-works.tsx
@@ -1,0 +1,31 @@
+'use client';
+
+import { InvestmentModelCard } from '~/components/shared/investment-model-card';
+import { SectionCaption } from '~/components/shared/section-caption';
+import { models } from '~/lib/mock-data/mock-how-it-works-section';
+
+export function HowItWorks() {
+  return (
+    <section className="w-full px-4 py-20 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl">
+        <SectionCaption
+          title="Secure, Transparent, and Powered by Web3"
+          subtitle="At KindFi, we ensure that every donation or contribution is backed by the security and transparency of a Web3-based Escrow system. Smart contracts guarantee that funds reach their intended destination to create real impact."
+          highlightWords={["Powered by Web3"]}
+        />
+
+        <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          {models.map((model, index) => (
+            <InvestmentModelCard
+              key={model.id}
+              {...model}
+              onLearnMore={() =>
+                console.log(`Learn more about model ${model.variant}`)
+              }
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};

apps/web/components/sections/home/join-us.tsx
@@ -1,0 +1,115 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { ChevronRight } from "lucide-react";
+import { Button } from "~/components/base/button";
+import { SectionCaption } from "~/components/shared/section-caption";
+import { fadeInUpAnimation } from "~/lib/constants/animations";
+import { features } from "~/lib/mock-data/mock-join-us-section";
+
+export function JoinUs() {
+  return (
+    <section className="relative py-24 overflow-hidden">
+      {/* Background */}
+      <div className="absolute inset-0">
+        <div className="absolute inset-0 bg-grid-slate-100/50 [mask-image:linear-gradient(0deg,white,rgba(255,255,255,0.6))]" />
+      </div>
+
+      <div className="relative container mx-auto px-4">
+        {/* Section Caption */}
+        <motion.div
+          {...fadeInUpAnimation}
+        >
+          <SectionCaption
+            title="Join the KindFi Revolution: Collaborate and Transform the World with Web3"
+            subtitle="The Web3 community has the power to give back. Support causes that matter, collaborate on impactful projects, and be part of a revolution leveraging Web3 technology to improve lives. Together, we can create lasting and meaningful change."
+            highlightWords={["Transform the World with Web3", "KindFi"]}
+          />
+        </motion.div>
+
+        {/* Feature Cards */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 mb-16">
+          {features.map((feature, index) => (
+            <motion.div
+              key={feature.id}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+            >
+              <article
+                className="group h-full bg-white rounded-2xl p-8 shadow-sm hover:shadow-md transition-all duration-300"
+                role="article"
+                aria-labelledby={`feature-title-${feature.id}`}
+              >
+                {/* Icon */}
+                {feature.icon}
+
+                {/* Content */}
+                <h3 id={`feature-title-${feature.id}`} className="mt-6 text-xl font-semibold text-gray-900">
+                  {feature.title}
+                </h3>
+                <p className="mt-4 text-gray-600 leading-relaxed">
+                  {feature.description}
+                </p>
+
+                {/* Highlight */}
+                <div className="mt-6 flex items-center text-sm font-medium text-blue-600">
+                  {feature.highlight}
+                  <ChevronRight className="w-4 h-4 ml-1 group-hover:translate-x-1 transition-transform duration-200" />
+                </div>
+              </article>
+            </motion.div>
+          ))}
+        </div>
+
+        {/* Call-to-Action Box */}
+        <motion.aside
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.6, delay: 0.3 }}
+          className="relative bg-white rounded-2xl p-8 lg:p-12 shadow-lg max-w-3xl mx-auto overflow-hidden"
+        >
+          {/* Background Pattern */}
+          <div className="absolute inset-0 bg-gradient-to-r from-blue-50 to-sky-50 opacity-50" />
+
+          <div className="relative">
+            <h4 className="text-xl font-semibold text-center text-gray-900 mb-4">
+              KindFi: One Better World
+            </h4>
+            <p className="text-gray-600 mb-8 leading-relaxed">
+              At KindFi, you’re part of a movement demonstrating that Web3 is
+              reshaping the future of social impact. From saving the planet to
+              empowering communities, every contribution makes a difference. Now
+              is the time to use decentralized power to drive true change. Join
+              us in making a real impact, free from the limitations of
+              traditional systems.
+            </p>
+            <div className="flex flex-col sm:flex-row gap-4 justify-center">
+              <Button
+                size="lg"
+                className="gradient-btn text-white px-8"
+                onClick={() => {
+                  // TODO: Implement join revolution action
+                }}
+              >
+                Join the Revolution
+              </Button>
+              <Button
+                size="lg"
+                variant="outline"
+                className="gradient-border-btn hover:bg-teal-50"
+                onClick={() => {
+                  // TODO: Implement discovery navigation
+                }}
+              >
+                Discover more about KindFi
+              </Button>
+            </div>
+          </div>
+        </motion.aside>
+      </div>
+    </section>
+  );
+};

apps/web/lib/mock-data/mock-community-section.tsx
@@ -1,0 +1,40 @@
+import { Rocket, Shield, TrendingUp, Users } from 'lucide-react';
+import type { Benefit, TestimonialData } from '~/lib/types/home.types';
+
+export const benefits: Benefit[] = [
+  {
+    id: 'community-social-impact',
+    icon: <Users className="w-5 h-5" />,
+    text: 'Community for Social Impact',
+  },
+  {
+    id: 'empowering-crypto-supporters',
+    icon: <TrendingUp className="w-5 h-5" />,
+    text: 'Empowering Crypto Supporters',
+  },
+  {
+    id: 'blockchain-verification',
+    icon: <Shield className="w-5 h-5" />,
+    text: 'Blockchain Verification and Security',
+  },
+  {
+    id: 'accelerating-blockchain-adoption',
+    icon: <Rocket className="w-5 h-5" />,
+    text: 'Accelerating Blockchain Adoption',
+  },
+];
+
+export const testimonialData: TestimonialData = {
+  quote: [
+    "The KindFi community becomes an ambassador for your social cause, ",
+    "taking your impact far beyond what traditional Web2 methods can achieve. ",
+    "Web3 connects and empowers people worldwide, creating a transparent, ",
+    "global, and secure network of support and verification powered by ",
+    "blockchain technology and Trustless work's Escrows. Supporting meaningful ",
+    "causes isn't something you can buy—it's a reward you earn by being part ",
+    "of a movement dedicated to real change."
+  ].join(""),
+  author: 'KindFi',
+  role: 'Social Impact Platform',
+  imageUrl: '/placeholder-image.jpg', // Replace with actual image
+};