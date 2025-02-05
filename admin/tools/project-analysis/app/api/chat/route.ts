Wardlabz/wardfund

Feat/implementation of project analysis assistant (#117)
* feat: add base to the project analysis assistant

* feat: add api route for the chat

* feat: add ai dependency

* feat: add ai sdk open ai dependency

* feat: add zod dependency

* feat: change the background and the color of letters in global css

* feat: change the main page of the chat

* feat: customize the users in the chat

* feat: add open ai edge dependency

* feat: add ai/react dependency

* feat: add functionality so it can read the main readme

* feat: add react markdown dependency

* feat: customize the styles and the global css file

* feat: add readme with all the research done and explanation of the implemented chatbot

---------

Co-authored-by: Brandon Fernández <31634868+Bran18@users.noreply.github.com>

admin/tools/project-analysis/app/api/chat/route.ts
@@ -1,0 +1,35 @@
+import { openai } from '@ai-sdk/openai'
+import { streamText } from 'ai'
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30
+
+export async function POST(req: Request) {
+  const { messages, readmeContent } = await req.json()
+
+  // Add system message with README content
+  const enhancedMessages = [
+    {
+      role: 'system',
+      content: `You are a knowledgeable assistant for WardFund, a comprehensive financial platform. 
+Here is the WardFund project documentation for context:
+
+${readmeContent}
+
+Use this information to provide accurate and enthusiastic responses about WardFund. 
+Always maintain a professional and positive tone when discussing WardFund's features and capabilities.
+Highlight WardFund's strengths and innovative approach to financial technology.
+If you don't know something specific, focus on what you do know about WardFund's general architecture and features.`
+    },
+    ...messages
+  ]
+
+  const result = streamText({
+    model: openai('gpt-4o'),
+    messages: enhancedMessages,
+    temperature: 0.7, // Add some creativity while maintaining accuracy
+    max_tokens: 500, // Ensure comprehensive responses
+  })
+
+  return result.toDataStreamResponse()
+}

admin/tools/project-analysis/app/page.tsx
@@ -1,0 +1,77 @@
+'use client';
+
+import { useChat } from 'ai/react';
+import { useEffect, useState } from 'react';
+
+export default function Chat() {
+  const [readmeContent, setReadmeContent] = useState('');
+
+  useEffect(() => {
+    // Fetch README content when component mounts
+    fetch('/api/readme')
+      .then(res => res.text())
+      .then(content => {
+        setReadmeContent(content);
+      })
+      .catch(console.error);
+  }, []);
+
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    initialMessages: [
+      {
+        id: 'welcome',
+        role: 'assistant',
+        content: 'Hi, here KindBot! I\'m your WardFund assistant. I can help you understand our project and answer any questions you have about WardFund. What would you like to know?'
+      }
+    ],
+    body: {
+      readmeContent, // Pass README content to the API
+    },
+  });
+
+  return (
+    <div className="min-h-screen from-zinc-100">
+      <div className="max-w-4xl mx-auto p-4 md:p-8">
+        <div className="mb-8">
+          <h1 className="text-3xl primary-foreground">WardFund Project Analysis</h1>
+          <p className="text-primary-600">Ask me anything about WardFund's architecture, features, or implementation details.</p>
+        </div>
+        
+        <div className="space-y-4 mb-4">
+          {messages.map(m => (
+            <div
+              key={m.id}
+              className={`p-4 rounded-lg ${
+                m.role === 'user'
+                  ? 'bg-green-500 text-white ml-auto max-w-[80%]'
+                  : 'border border-zinc-200 dark:border-zinc-600 max-w-[80%]'
+              }`}
+            >
+              <div className="whitespace-pre-wrap">
+                {m.content}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <form onSubmit={handleSubmit} className="fixed bottom-0 left-0 right-0 p-4 border-t border-zinc-200 dark:border-zinc-700">
+          <div className="max-w-4xl mx-auto flex gap-4">
+            <input
+              className="flex-1 p-4 rounded-lg border border-zinc-300 dark:border-zinc-600 text-black focus:outline-none focus:ring-2 focus:ring-green-500 dark:focus:ring-green-400"
+              value={input}
+              placeholder="Ask about WardFund..."
+              onChange={handleInputChange}
+            />
+            <button
+              type="submit"
+              className="px-6 py-4 bg-green-400 hover:bg-green-500 text-white font-semibold rounded-lg transition-colors"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+

kindfi-org/kindfi

admin/tools/project-analysis/app/api/chat/route.ts
@@ -1,0 +1,35 @@
+import { openai } from '@ai-sdk/openai'
+import { streamText } from 'ai'
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30
+
+export async function POST(req: Request) {
+  const { messages, readmeContent } = await req.json()
+
+  // Add system message with README content
+  const enhancedMessages = [
+    {
+      role: 'system',
+      content: `You are a knowledgeable assistant for KindFi, a comprehensive financial platform. 
+Here is the KindFi project documentation for context:
+
+${readmeContent}
+
+Use this information to provide accurate and enthusiastic responses about KindFi. 
+Always maintain a professional and positive tone when discussing KindFi's features and capabilities.
+Highlight KindFi's strengths and innovative approach to financial technology.
+If you don't know something specific, focus on what you do know about KindFi's general architecture and features.`
+    },
+    ...messages
+  ]
+
+  const result = streamText({
+    model: openai('gpt-4o'),
+    messages: enhancedMessages,
+    temperature: 0.7, // Add some creativity while maintaining accuracy
+    max_tokens: 500, // Ensure comprehensive responses
+  })
+
+  return result.toDataStreamResponse()
+}

admin/tools/project-analysis/app/page.tsx
@@ -1,0 +1,77 @@
+'use client';
+
+import { useChat } from 'ai/react';
+import { useEffect, useState } from 'react';
+
+export default function Chat() {
+  const [readmeContent, setReadmeContent] = useState('');
+
+  useEffect(() => {
+    // Fetch README content when component mounts
+    fetch('/api/readme')
+      .then(res => res.text())
+      .then(content => {
+        setReadmeContent(content);
+      })
+      .catch(console.error);
+  }, []);
+
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    initialMessages: [
+      {
+        id: 'welcome',
+        role: 'assistant',
+        content: 'Hi, here KindBot! I\'m your KindFi assistant. I can help you understand our project and answer any questions you have about KindFi. What would you like to know?'
+      }
+    ],
+    body: {
+      readmeContent, // Pass README content to the API
+    },
+  });
+
+  return (
+    <div className="min-h-screen from-zinc-100">
+      <div className="max-w-4xl mx-auto p-4 md:p-8">
+        <div className="mb-8">
+          <h1 className="text-3xl primary-foreground">KindFi Project Analysis</h1>
+          <p className="text-primary-600">Ask me anything about KindFi's architecture, features, or implementation details.</p>
+        </div>
+        
+        <div className="space-y-4 mb-4">
+          {messages.map(m => (
+            <div
+              key={m.id}
+              className={`p-4 rounded-lg ${
+                m.role === 'user'
+                  ? 'bg-green-500 text-white ml-auto max-w-[80%]'
+                  : 'border border-zinc-200 dark:border-zinc-600 max-w-[80%]'
+              }`}
+            >
+              <div className="whitespace-pre-wrap">
+                {m.content}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <form onSubmit={handleSubmit} className="fixed bottom-0 left-0 right-0 p-4 border-t border-zinc-200 dark:border-zinc-700">
+          <div className="max-w-4xl mx-auto flex gap-4">
+            <input
+              className="flex-1 p-4 rounded-lg border border-zinc-300 dark:border-zinc-600 text-black focus:outline-none focus:ring-2 focus:ring-green-500 dark:focus:ring-green-400"
+              value={input}
+              placeholder="Ask about KindFi..."
+              onChange={handleInputChange}
+            />
+            <button
+              type="submit"
+              className="px-6 py-4 bg-green-400 hover:bg-green-500 text-white font-semibold rounded-lg transition-colors"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+