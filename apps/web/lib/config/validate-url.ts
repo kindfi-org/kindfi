Wardlabz/wardfund

fix: improve auth error handling (#107)
* fix: improve auth error handling

* fix: improve auth error handling

* fix:  fix merge conflict from develop branch

* fix: type defintions for logger.ts an remove duplicate animation.ts

* fix build failure

* fix build failure

* fix build error

apps/web/lib/config/validate-url.ts
@@ -1,0 +1,31 @@
+import { Logger } from "../logger"
+
+const logger = new Logger()
+// List of allowed domains for redirection
+export const ALLOWED_DOMAINS = [
+    'wardfund.org',
+    'wardfund.vercel.app',
+    'kind-fi.com',
+    /^wardfund-[\w-]+\.vercel\.app$/, // Dynamic subdomains
+  ]
+  
+  // Function to validate redirect URLs
+  export function isValidRedirectUrl(url: string): boolean {
+    try {
+      const parsedUrl = new URL(url)
+      // Check if the hostname matches any allowed domain
+      return ALLOWED_DOMAINS.some((domain) =>
+        typeof domain === 'string'
+          ? parsedUrl.hostname === domain
+          : domain.test(parsedUrl.hostname),
+      )
+    } catch (error) {
+      logger.error({
+        eventType: 'URL_VALIDATION_ERROR',
+        error: error instanceof Error ? error.message : 'Unknown error',
+        url,
+        timestamp: new Date().toISOString(),
+      })
+      return false
+    }
+  }

kindfi-org/kindfi

apps/web/lib/config/validate-url.ts
@@ -1,0 +1,31 @@
+import { Logger } from "../logger"
+
+const logger = new Logger()
+// List of allowed domains for redirection
+export const ALLOWED_DOMAINS = [
+    'kindfi.org',
+    'kindfi.vercel.app',
+    'kind-fi.com',
+    /^kindfi-[\w-]+\.vercel\.app$/, // Dynamic subdomains
+  ]
+  
+  // Function to validate redirect URLs
+  export function isValidRedirectUrl(url: string): boolean {
+    try {
+      const parsedUrl = new URL(url)
+      // Check if the hostname matches any allowed domain
+      return ALLOWED_DOMAINS.some((domain) =>
+        typeof domain === 'string'
+          ? parsedUrl.hostname === domain
+          : domain.test(parsedUrl.hostname),
+      )
+    } catch (error) {
+      logger.error({
+        eventType: 'URL_VALIDATION_ERROR',
+        error: error instanceof Error ? error.message : 'Unknown error',
+        url,
+        timestamp: new Date().toISOString(),
+      })
+      return false
+    }
+  }