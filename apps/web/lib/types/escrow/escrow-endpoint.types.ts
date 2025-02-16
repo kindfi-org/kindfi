657903450868bdeec68b6542ba7b17da5613ea36
Joel Vargas <vermudo.com@gmail.com>
2025-02-16 15:54:33 -0600

    feat(escrow): add new types for escrow endpoint actions and request structure

---
+++ apps/web/lib/types/escrow/escrow-endpoint.types.ts
@@ -0,0 +1,17 @@
+import { HttpMethod } from "../utils.types";
+
+export type EscrowEndpoint =
+  | "initiate"
+  | "fund"
+  | "dispute"
+  | "resolve"
+  | "release"
+  | "completeMilestone"
+  | "approveMilestone";
+
+export type TCreateEscrowRequest<T extends EscrowEndpoint> = {
+  action: T;
+  method: HttpMethod;
+  data?: Record<string, any>;
+  params?: Record<string, any>;
+};
