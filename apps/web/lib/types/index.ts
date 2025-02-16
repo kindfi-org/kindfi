eb8f0be95316ace8162fe97c3ecf6f1785a813e3
Joel Vargas <vermudo.com@gmail.com>
2025-02-16 15:53:41 -0600

    refactor(types): update export paths for improved organization

--- apps/web/lib/types/index.ts
+++ apps/web/lib/types/index.ts
@@ -1,16 +1,16 @@
 export type ConditionalRequired<
-	T,
-	K extends keyof T,
-	Flag extends boolean,
-> = Flag extends true ? Required<Pick<T, K>> : Partial<Pick<T, K>>
+  T,
+  K extends keyof T,
+  Flag extends boolean,
+> = Flag extends true ? Required<Pick<T, K>> : Partial<Pick<T, K>>;
 
-export * from './escrow.types'
-export * from './home.types'
-export * from './logger.types'
-export * from './pages.type'
-export * from './passkey.types'
-export * from './project.types'
-export * from './right-side-panel.types'
-export * from './section.types'
-export * from './stats.types'
-export * from './user-dashboard.types'
+export * from "./escrow/escrow.types";
+export * from "./home.types";
+export * from "./logger.types";
+export * from "./pages.type";
+export * from "./passkey.types";
+export * from "./project.types";
+export * from "./right-side-panel.types";
+export * from "./section.types";
+export * from "./stats.types";
+export * from "./user-dashboard.types";
