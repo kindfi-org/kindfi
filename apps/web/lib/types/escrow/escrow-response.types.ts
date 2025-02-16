1360e84934592cf656da39d35a79c369b0dee2af
Joel Vargas <vermudo.com@gmail.com>
2025-02-16 15:53:49 -0600

    feat(escrow): add response types for escrow requests and transactions

---
+++ apps/web/lib/types/escrow/escrow-response.types.ts
@@ -0,0 +1,23 @@
+import { Status } from "../utils.types";
+import { EscrowPayload } from "./escrow-payload.types";
+
+// Escrow's Response
+export type EscrowRequestResponse = {
+  status: Status;
+  unsignedTransaction?: string;
+};
+
+export type InitializeEscrowResponse = {
+  contract_id: string;
+  escrow: EscrowPayload;
+  message: string;
+  status: Status;
+};
+
+export type FundEscrowResponse = {
+  message: string;
+  status: Status;
+};
+
+export type SendTransactionResponse = InitializeEscrowResponse &
+  FundEscrowResponse;
