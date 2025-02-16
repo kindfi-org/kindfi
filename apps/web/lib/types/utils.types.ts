709b21fadb45a9f9211a1c090fac0ff4806b78ff
Joel Vargas <vermudo.com@gmail.com>
2025-02-16 15:54:30 -0600

    refactor(types): remove unused escrow endpoint types and request structure

--- apps/web/lib/types/utils.types.ts
+++ apps/web/lib/types/utils.types.ts
@@ -1,19 +1,3 @@
 export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
 
 export type Status = "SUCCESS" | "ERROR";
-
-export type EscrowEndpoint =
-  | "initiate"
-  | "fund"
-  | "dispute"
-  | "resolve"
-  | "release"
-  | "completeMilestone"
-  | "approveMilestone";
-
-export type TCreateEscrowRequest<T extends EscrowEndpoint> = {
-  action: T;
-  method: HttpMethod;
-  data?: Record<string, any>;
-  params?: Record<string, any>;
-};
