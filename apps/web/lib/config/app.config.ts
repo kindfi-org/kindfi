512c58d227d8f17bb4c3aaf137ce9eaf0ca551f9
Roberto Lucas <contact@andler.dev>
2025-02-19 02:40:04 -0600

    fix: bun build (#175)
    * fix: rm package-lock file
    
    * chore: clean up + tweaks

--- apps/web/lib/config/app.config.ts
+++ apps/web/lib/config/app.config.ts
@@ -1,5 +1,7 @@
 export const appConfig = {
-    features: {
-      enableEscrowFeature: process.env.NODE_ENV === 'development' || process.env.NEXT_PUBLIC_ENABLE_ESCROW_FEATURE === 'true'
-    }
-  } as const
+	features: {
+		enableEscrowFeature:
+			process.env.NODE_ENV === 'development' ||
+			process.env.NEXT_PUBLIC_ENABLE_ESCROW_FEATURE === 'true',
+	},
+} as const
