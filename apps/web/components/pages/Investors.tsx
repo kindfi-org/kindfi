6db32435e5facbcbb07bd2f15a736ff2a5643afa
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-02-26 06:21:28 +0000

    [web]refactor: remove unused skeleton component imports(#183)

--- apps/web/components/pages/Investors.tsx
+++ apps/web/components/pages/Investors.tsx
@@ -2,9 +2,6 @@ import dynamic from 'next/dynamic'
 import {
 	SkeletonCommunityVoices,
 	SkeletonHero,
-	SkeletonImpactCategories,
-	SkeletonImpactMakers,
-	SkeletonMakeImpact,
 	SkeletonSuccessStories,
 } from '~/components/sections/impact/skeletons'
 
