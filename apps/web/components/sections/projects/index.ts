b119daaf1a9b1cba5d6e9fe82b1d31907688ef44
Anouk Rímola <77553677+AnoukRImola@users.noreply.github.com>
2025-02-27 00:06:17 -0500

    Feat: Create projects Section (#219)
    * Feat: Create projects Section
    
    * fix: build issues
    
    ---------
    
    Co-authored-by: Brandon Fernández <31634868+Bran18@users.noreply.github.com>
    Co-authored-by: bran18 <andreyfdez18@gmail.com>

---
+++ apps/web/components/sections/projects/index.ts
@@ -0,0 +1,4 @@
+export { CategoryFilter } from './category-filter'
+export { ProjectsHeader } from './projects-header'
+export { ProjectsGrid } from './projects-grid'
+export { SortDropdown } from './sort-dropdown'
