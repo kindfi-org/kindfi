096722684b929c39dfcc9e9630d0135847dc1ec2
Bioba <97415163+Agbeleshe@users.noreply.github.com>
2025-02-28 20:02:31 +0100

    Enhance rich text editor link interface with a custom dialog component #222 (#247)
    * signed commit
    
    * fix: add validate env
    
    ---------
    
    Co-authored-by: bran18 <andreyfdez18@gmail.com>

--- apps/web/lib/supabase/check-env-vars.ts
+++ apps/web/lib/supabase/check-env-vars.ts
@@ -1,19 +1,20 @@
 export function validateEnvVars(requiredEnvVars: string[]) {
-  requiredEnvVars.forEach((envVar) => {
-    if (!process.env[envVar]) {
-      throw new Error(`Missing required environment variable: ${envVar}`);
-    }
-  });
+	// biome-ignore lint/complexity/noForEach: <explanation>
+	requiredEnvVars.forEach((envVar) => {
+		if (!process.env[envVar]) {
+			throw new Error(`Missing required environment variable: ${envVar}`)
+		}
+	})
 
-  console.log("All required environment variables are present.");
+	console.log('All required environment variables are present.')
 }
 
 export function validateSupabaseEnvVars() {
-  const requiredEnvVars = [
-    "SUPABASE_URL",
-    "SUPABASE_ANON_KEY",
-    "SUPABASE_SERVICE_ROLE_KEY",
-  ];
+	const requiredEnvVars = [
+		'SUPABASE_URL',
+		'SUPABASE_ANON_KEY',
+		'SUPABASE_SERVICE_ROLE_KEY',
+	]
 
-  validateEnvVars(requiredEnvVars);
+	validateEnvVars(requiredEnvVars)
 }
