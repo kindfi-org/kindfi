a9ba015f7bad8de4dea2d2c1c770187136c9f9d3
Francisco Campos Díaz <sasasamaes@users.noreply.github.com>
2025-02-28 14:25:50 -0600

    Feat/kyc-1 (#249)
    * fix keys
    
    * delete react-calendar and react date picker
    
    * add alert msj in form
    
    * fix: build issue
    
    ---------
    
    Co-authored-by: Brandon Fernández <31634868+Bran18@users.noreply.github.com>
    Co-authored-by: bran18 <andreyfdez18@gmail.com>

---
+++ apps/web/components/shared/kyc/kyc-1/date-of-birth-field.tsx
@@ -0,0 +1,73 @@
+import { format } from 'date-fns'
+import { CalendarIcon } from 'lucide-react'
+import { Button } from '~/components/base/button'
+import { Calendar } from '~/components/base/calendar'
+import {
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '~/components/base/form'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '~/components/base/popover'
+import { cn } from '~/lib/utils'
+import type { FormFieldProps } from './types'
+
+export function DateOfBirthField({ control }: FormFieldProps) {
+	const formatDate = (date: Date) => {
+		try {
+			return format(date, 'PPP')
+		} catch (error) {
+			return 'Invalid date'
+		}
+	}
+
+	return (
+		<FormField
+			control={control}
+			name="dateOfBirth"
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel className="text-lg font-medium">Date of Birth</FormLabel>
+					<Popover>
+						<PopoverTrigger asChild>
+							<Button
+								variant="outline"
+								className={cn(
+									'w-full justify-start text-left font-normal',
+									!field.value && 'text-muted-foreground',
+								)}
+							>
+								<CalendarIcon className="mr-2 h-4 w-4" />
+								{field.value ? formatDate(field.value) : 'Pick a date'}
+							</Button>
+						</PopoverTrigger>
+						<PopoverContent className="w-auto p-0" align="start">
+							<Calendar
+								mode="single"
+								selected={field.value}
+								onSelect={field.onChange}
+								disabled={(date) => {
+									const today = new Date()
+									const eighteenYearsAgo = new Date(
+										today.getFullYear() - 18,
+										today.getMonth(),
+										today.getDate(),
+									)
+									return date > eighteenYearsAgo
+								}}
+							/>
+						</PopoverContent>
+					</Popover>
+					<p className="text-sm text-gray-500 mt-1">
+						You must be at least 18 years old
+					</p>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	)
+}
