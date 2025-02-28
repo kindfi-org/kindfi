fe231bce7dd03cf1c3b713e469082121ecbd8154
jahrulezfrancis <longsaarmuknaan@gmail.com>
2025-02-28 09:40:48 +0100

    fix: updating types

--- apps/web/lib/types/projects.types.ts
+++ apps/web/lib/types/projects.types.ts
@@ -1,5 +1,6 @@
 import type { LucideIcon } from 'lucide-react'
 import { Tag } from '~/components/shared/project-card'
+import { Creator } from './featured-projects/featured-projects.types'
 
 export interface ProjectCategory {
 	id: string
@@ -17,9 +18,15 @@ export interface Project {
 	categories: string[]
 	currentAmount: number
 	targetAmount: number
-	supporters: number
-	minSupport: number
+	investors: number
+	milestones: number
+	completedMilestones: number
+	minimumSupport: number
+	creator: Creator
+	category: string
 	tags: Tag[] | string[]
 	createdAt: string
 	location?: string
+	trending: boolean
+	featured: boolean
 }
