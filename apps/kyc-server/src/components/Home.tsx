Wardlabz/wardfund

feat: Refactor: Express.js to Bun native server with React SSR Integration (#254)
* feat: add ssr and replace express

* docs: it closes #164

apps/kyc-server/src/components/Home.tsx
@@ -1,0 +1,97 @@
+import { existsSync, readFileSync } from 'node:fs'
+import { join } from 'node:path'
+import React from 'react'
+import type { ReactNode } from 'react'
+import { Navigation } from './Navigation'
+
+interface HomeProps {
+	message: string | ReactNode
+	currentPath?: string
+}
+
+// Get the client filename from the manifest
+function getClientFilename(): string {
+	try {
+		const manifestPath = join(process.cwd(), 'public', 'manifest.json')
+		if (existsSync(manifestPath)) {
+			const manifest = JSON.parse(readFileSync(manifestPath, 'utf-8'))
+			return manifest.clientJs || 'client.js'
+		}
+	} catch (error) {
+		console.error('Error reading manifest:', error)
+	}
+	return 'client.js'
+}
+
+export function Home({ message, currentPath = '/' }: HomeProps) {
+	// Get the client filename
+	const clientJs = getClientFilename()
+
+	return (
+		<html lang="en">
+			<head>
+				<title>WardFund KYC Server</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<style>{`
+          body {
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 2rem;
+            line-height: 1.6;
+          }
+          h1 {
+            color: #333;
+            margin-bottom: 1rem;
+          }
+          p {
+            color: #666;
+            margin-bottom: 1.5rem;
+          }
+          .card {
+            background: #f9f9f9;
+            border-radius: 8px;
+            padding: 1.5rem;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            margin-bottom: 1.5rem;
+          }
+          nav {
+            margin-bottom: 2rem;
+          }
+          nav ul {
+            display: flex;
+            list-style: none;
+            padding: 0;
+            gap: 1rem;
+          }
+          nav a {
+            color: #0066cc;
+            text-decoration: none;
+            padding: 0.5rem 0;
+            border-bottom: 2px solid transparent;
+            transition: border-color 0.2s;
+          }
+          nav a:hover {
+            border-color: #0066cc;
+          }
+          nav a.active {
+            border-color: #0066cc;
+            font-weight: 500;
+          }
+        `}</style>
+				<script type="module" src={`/${clientJs}`} defer />
+			</head>
+			<body>
+				<h1>WardFund KYC Server</h1>
+				<div id="root">
+					<Navigation currentPath={currentPath} />
+					<div className="card">
+						{typeof message === 'string' ? <p>{message}</p> : message}
+					</div>
+					<p>Server-side rendered with React and Bun</p>
+				</div>
+			</body>
+		</html>
+	)
+}

apps/kyc-server/src/config/cors.ts
@@ -1,0 +1,29 @@
+import type { CorsOptions } from '../middleware/cors'
+
+/**
+ * CORS configuration for the application
+ *
+ * This can be modified to restrict access to specific domains
+ * Examples:
+ * - ['https://example.com', 'https://api.example.com'] - Allow only these domains
+ * - ['*.example.com'] - Allow all subdomains of example.com
+ * - '*' - Allow all origins (not recommended for production)
+ */
+export const corsConfig: CorsOptions = {
+	// Set allowed origins based on environment
+	allowedOrigins:
+		process.env.NODE_ENV === 'production'
+			? process.env.ALLOWED_ORIGINS
+				? process.env.ALLOWED_ORIGINS.split(',')
+				: ['https://app.wardfund.com']
+			: '*',
+
+	// HTTP methods allowed
+	allowedMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+
+	// HTTP headers allowed
+	allowedHeaders: ['Content-Type', 'Authorization'],
+
+	// Cache duration for preflight requests in seconds (24 hours)
+	maxAge: 86400,
+}

apps/kyc-server/src/index.tsx
@@ -1,0 +1,225 @@
+import { existsSync, readFileSync } from 'node:fs'
+import { join } from 'node:path'
+import { serve } from 'bun'
+import type { Server, ServerWebSocket } from 'bun'
+import { routes } from './routes'
+import { buildClient } from './utils/buildClient'
+
+interface ClientData {
+	clientId: string
+	joinedAt: string
+}
+
+interface ParsedMessage {
+	text?: string
+	type?: string
+	[key: string]: unknown
+}
+
+// Function to get the current client filename from manifest
+function getClientFilename(): string {
+	try {
+		const manifestPath = join(process.cwd(), 'public', 'manifest.json')
+		if (existsSync(manifestPath)) {
+			const manifest = JSON.parse(readFileSync(manifestPath, 'utf-8'))
+			return manifest.clientJs || 'client.js'
+		}
+	} catch (error) {
+		console.error('Error reading manifest:', error)
+	}
+	return 'client.js'
+}
+
+async function startServer() {
+	// Build the client-side JavaScript first
+	await buildClient()
+
+	// Keep track of connected clients
+	// TODO: this is a demonstration of how to use the server, it should be replaced with a real database
+	const clients = new Set<ServerWebSocket<ClientData>>()
+
+	// Define server options with type assertion
+	const serverOptions = {
+		fetch(req: Request, server: Server): Response | Promise<Response> {
+			const url = new URL(req.url)
+
+			// Handle WebSocket connections
+			if (url.pathname === '/live') {
+				const upgraded = server.upgrade<ClientData>(req, {
+					// Data passed to the WebSocket object
+					data: {
+						clientId: crypto.randomUUID(),
+						joinedAt: new Date().toISOString(),
+					},
+				})
+
+				if (!upgraded) {
+					return new Response('Upgrade failed', { status: 400 })
+				}
+
+				return new Response()
+			}
+
+			// Serve static files from the public directory
+			if (url.pathname.startsWith('/client') && url.pathname.endsWith('.js')) {
+				try {
+					// Get the requested filename from the URL
+					const requestedFile = url.pathname.substring(1) // Remove leading slash
+
+					// Get the path to the public directory
+					const publicDir = join(process.cwd(), 'public')
+
+					// Check if the specific file exists
+					const specificFilePath = join(publicDir, requestedFile)
+					if (existsSync(specificFilePath)) {
+						const file = Bun.file(specificFilePath)
+						return new Response(file, {
+							headers: {
+								'Content-Type': 'application/javascript',
+								'Cache-Control': 'max-age=3600',
+							},
+						})
+					}
+
+					// If specific file not found, try to serve the current client file from manifest
+					const currentClientJs = getClientFilename()
+					const filePath = join(publicDir, currentClientJs)
+					if (existsSync(filePath)) {
+						const file = Bun.file(filePath)
+						return new Response(file, {
+							headers: {
+								'Content-Type': 'application/javascript',
+								'Cache-Control': 'max-age=3600',
+							},
+						})
+					}
+
+					throw new Error('Client JavaScript file not found')
+				} catch (error) {
+					console.error('Error serving static file:', error)
+					return new Response('File not found', { status: 404 })
+				}
+			}
+
+			// Check if the route exists in our routes object
+			const pathname = url.pathname
+			const route = routes[pathname as keyof typeof routes]
+
+			if (route) {
+				const method = req.method
+				if (method in route) {
+					// @ts-expect-error method is dynamically accessed
+					return route[method](req)
+				}
+
+				// Method not allowed
+				return new Response('Method not allowed', { status: 405 })
+			}
+
+			// Default response for unknown routes
+			return new Response('WardFund KYC Server API', { status: 404 })
+		},
+		websocket: {
+			open(ws: ServerWebSocket<ClientData>) {
+				// Add the client to the set of connected clients
+				clients.add(ws)
+
+				// Log the connection
+				console.log(`Client connected: ${ws.data.clientId}`)
+
+				// Send a welcome message to the client
+				ws.send(
+					JSON.stringify({
+						type: 'welcome',
+						message: `Welcome to the WardFund KYC Server! Your client ID is ${ws.data.clientId}`,
+						clientId: ws.data.clientId,
+					}),
+				)
+
+				// Broadcast to all clients that a new user has joined
+				for (const client of clients) {
+					if (client !== ws) {
+						client.send(
+							JSON.stringify({
+								type: 'userJoined',
+								message: `A new user has joined! Total users: ${clients.size}`,
+								totalUsers: clients.size,
+							}),
+						)
+					}
+				}
+			},
+			message(ws: ServerWebSocket<ClientData>, message: string) {
+				// Log the message
+				console.log(`Message from ${ws.data.clientId}: ${message}`)
+
+				try {
+					// Parse the message
+					const parsedMessage = JSON.parse(message) as ParsedMessage
+
+					// Prepare response message
+					const responseMessage = {
+						type: 'message',
+						clientId: ws.data.clientId,
+						message: parsedMessage.text || message,
+						text: parsedMessage.text || message,
+						timestamp: new Date().toISOString(),
+					}
+
+					// Broadcast the message to all clients
+					for (const client of clients) {
+						client.send(JSON.stringify(responseMessage))
+					}
+				} catch (error) {
+					// If the message is not valid JSON, just broadcast it as is
+					console.error('Error parsing message:', error)
+
+					// Prepare response for plain text message
+					const responseMessage = {
+						type: 'message',
+						clientId: ws.data.clientId,
+						message: message,
+						text: message,
+						timestamp: new Date().toISOString(),
+					}
+
+					// Broadcast to all clients
+					for (const client of clients) {
+						client.send(JSON.stringify(responseMessage))
+					}
+				}
+			},
+			close(ws: ServerWebSocket<ClientData>) {
+				// Remove the client from the set of connected clients
+				clients.delete(ws)
+
+				// Log the disconnection
+				console.log(`Client disconnected: ${ws.data.clientId}`)
+
+				// Broadcast to all clients that a user has left
+				for (const client of clients) {
+					client.send(
+						JSON.stringify({
+							type: 'userLeft',
+							message: `A user has left. Total users: ${clients.size}`,
+							totalUsers: clients.size,
+						}),
+					)
+				}
+			},
+		},
+		port: process.env.PORT ? Number.parseInt(process.env.PORT) : 3001,
+		routes,
+		development: process.env.NODE_ENV !== 'production',
+	} as const
+
+	// Start the server
+	const server = serve(serverOptions)
+	console.log(`🚀 Server running at http://localhost:${server.port}/`)
+}
+
+// Start the server
+startServer().catch((error) => {
+	console.error('Failed to start server:', error)
+	process.exit(1)
+})

apps/kyc-server/src/utils/contentMap.tsx
@@ -1,0 +1,20 @@
+import React from 'react'
+import type { ReactNode } from 'react'
+import { About } from '../components/About'
+import { WebSocketDemo } from '../components/WebSocketDemo'
+
+// Centralized content map for both client and server
+export const contentMap: Record<string, ReactNode> = {
+	'/': 'Welcome to WardFund KYC Server!',
+	'/react': 'This page is server-side rendered with React and Bun!',
+	'/websocket': <WebSocketDemo />,
+	'/about': <About />,
+}
+
+// Helper function to get content with a fallback
+export function getContent(
+	path: string,
+	fallback: ReactNode = 'Welcome to WardFund KYC Server!',
+): ReactNode {
+	return contentMap[path] || fallback
+}

kindfi-org/kindfi

apps/kyc-server/src/components/Home.tsx
@@ -1,0 +1,97 @@
+import { existsSync, readFileSync } from 'node:fs'
+import { join } from 'node:path'
+import React from 'react'
+import type { ReactNode } from 'react'
+import { Navigation } from './Navigation'
+
+interface HomeProps {
+	message: string | ReactNode
+	currentPath?: string
+}
+
+// Get the client filename from the manifest
+function getClientFilename(): string {
+	try {
+		const manifestPath = join(process.cwd(), 'public', 'manifest.json')
+		if (existsSync(manifestPath)) {
+			const manifest = JSON.parse(readFileSync(manifestPath, 'utf-8'))
+			return manifest.clientJs || 'client.js'
+		}
+	} catch (error) {
+		console.error('Error reading manifest:', error)
+	}
+	return 'client.js'
+}
+
+export function Home({ message, currentPath = '/' }: HomeProps) {
+	// Get the client filename
+	const clientJs = getClientFilename()
+
+	return (
+		<html lang="en">
+			<head>
+				<title>Kindfi KYC Server</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<style>{`
+          body {
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 2rem;
+            line-height: 1.6;
+          }
+          h1 {
+            color: #333;
+            margin-bottom: 1rem;
+          }
+          p {
+            color: #666;
+            margin-bottom: 1.5rem;
+          }
+          .card {
+            background: #f9f9f9;
+            border-radius: 8px;
+            padding: 1.5rem;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            margin-bottom: 1.5rem;
+          }
+          nav {
+            margin-bottom: 2rem;
+          }
+          nav ul {
+            display: flex;
+            list-style: none;
+            padding: 0;
+            gap: 1rem;
+          }
+          nav a {
+            color: #0066cc;
+            text-decoration: none;
+            padding: 0.5rem 0;
+            border-bottom: 2px solid transparent;
+            transition: border-color 0.2s;
+          }
+          nav a:hover {
+            border-color: #0066cc;
+          }
+          nav a.active {
+            border-color: #0066cc;
+            font-weight: 500;
+          }
+        `}</style>
+				<script type="module" src={`/${clientJs}`} defer />
+			</head>
+			<body>
+				<h1>Kindfi KYC Server</h1>
+				<div id="root">
+					<Navigation currentPath={currentPath} />
+					<div className="card">
+						{typeof message === 'string' ? <p>{message}</p> : message}
+					</div>
+					<p>Server-side rendered with React and Bun</p>
+				</div>
+			</body>
+		</html>
+	)
+}

apps/kyc-server/src/config/cors.ts
@@ -1,0 +1,29 @@
+import type { CorsOptions } from '../middleware/cors'
+
+/**
+ * CORS configuration for the application
+ *
+ * This can be modified to restrict access to specific domains
+ * Examples:
+ * - ['https://example.com', 'https://api.example.com'] - Allow only these domains
+ * - ['*.example.com'] - Allow all subdomains of example.com
+ * - '*' - Allow all origins (not recommended for production)
+ */
+export const corsConfig: CorsOptions = {
+	// Set allowed origins based on environment
+	allowedOrigins:
+		process.env.NODE_ENV === 'production'
+			? process.env.ALLOWED_ORIGINS
+				? process.env.ALLOWED_ORIGINS.split(',')
+				: ['https://app.kindfi.com']
+			: '*',
+
+	// HTTP methods allowed
+	allowedMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+
+	// HTTP headers allowed
+	allowedHeaders: ['Content-Type', 'Authorization'],
+
+	// Cache duration for preflight requests in seconds (24 hours)
+	maxAge: 86400,
+}

apps/kyc-server/src/index.tsx
@@ -1,0 +1,225 @@
+import { existsSync, readFileSync } from 'node:fs'
+import { join } from 'node:path'
+import { serve } from 'bun'
+import type { Server, ServerWebSocket } from 'bun'
+import { routes } from './routes'
+import { buildClient } from './utils/buildClient'
+
+interface ClientData {
+	clientId: string
+	joinedAt: string
+}
+
+interface ParsedMessage {
+	text?: string
+	type?: string
+	[key: string]: unknown
+}
+
+// Function to get the current client filename from manifest
+function getClientFilename(): string {
+	try {
+		const manifestPath = join(process.cwd(), 'public', 'manifest.json')
+		if (existsSync(manifestPath)) {
+			const manifest = JSON.parse(readFileSync(manifestPath, 'utf-8'))
+			return manifest.clientJs || 'client.js'
+		}
+	} catch (error) {
+		console.error('Error reading manifest:', error)
+	}
+	return 'client.js'
+}
+
+async function startServer() {
+	// Build the client-side JavaScript first
+	await buildClient()
+
+	// Keep track of connected clients
+	// TODO: this is a demonstration of how to use the server, it should be replaced with a real database
+	const clients = new Set<ServerWebSocket<ClientData>>()
+
+	// Define server options with type assertion
+	const serverOptions = {
+		fetch(req: Request, server: Server): Response | Promise<Response> {
+			const url = new URL(req.url)
+
+			// Handle WebSocket connections
+			if (url.pathname === '/live') {
+				const upgraded = server.upgrade<ClientData>(req, {
+					// Data passed to the WebSocket object
+					data: {
+						clientId: crypto.randomUUID(),
+						joinedAt: new Date().toISOString(),
+					},
+				})
+
+				if (!upgraded) {
+					return new Response('Upgrade failed', { status: 400 })
+				}
+
+				return new Response()
+			}
+
+			// Serve static files from the public directory
+			if (url.pathname.startsWith('/client') && url.pathname.endsWith('.js')) {
+				try {
+					// Get the requested filename from the URL
+					const requestedFile = url.pathname.substring(1) // Remove leading slash
+
+					// Get the path to the public directory
+					const publicDir = join(process.cwd(), 'public')
+
+					// Check if the specific file exists
+					const specificFilePath = join(publicDir, requestedFile)
+					if (existsSync(specificFilePath)) {
+						const file = Bun.file(specificFilePath)
+						return new Response(file, {
+							headers: {
+								'Content-Type': 'application/javascript',
+								'Cache-Control': 'max-age=3600',
+							},
+						})
+					}
+
+					// If specific file not found, try to serve the current client file from manifest
+					const currentClientJs = getClientFilename()
+					const filePath = join(publicDir, currentClientJs)
+					if (existsSync(filePath)) {
+						const file = Bun.file(filePath)
+						return new Response(file, {
+							headers: {
+								'Content-Type': 'application/javascript',
+								'Cache-Control': 'max-age=3600',
+							},
+						})
+					}
+
+					throw new Error('Client JavaScript file not found')
+				} catch (error) {
+					console.error('Error serving static file:', error)
+					return new Response('File not found', { status: 404 })
+				}
+			}
+
+			// Check if the route exists in our routes object
+			const pathname = url.pathname
+			const route = routes[pathname as keyof typeof routes]
+
+			if (route) {
+				const method = req.method
+				if (method in route) {
+					// @ts-expect-error method is dynamically accessed
+					return route[method](req)
+				}
+
+				// Method not allowed
+				return new Response('Method not allowed', { status: 405 })
+			}
+
+			// Default response for unknown routes
+			return new Response('Kindfi KYC Server API', { status: 404 })
+		},
+		websocket: {
+			open(ws: ServerWebSocket<ClientData>) {
+				// Add the client to the set of connected clients
+				clients.add(ws)
+
+				// Log the connection
+				console.log(`Client connected: ${ws.data.clientId}`)
+
+				// Send a welcome message to the client
+				ws.send(
+					JSON.stringify({
+						type: 'welcome',
+						message: `Welcome to the Kindfi KYC Server! Your client ID is ${ws.data.clientId}`,
+						clientId: ws.data.clientId,
+					}),
+				)
+
+				// Broadcast to all clients that a new user has joined
+				for (const client of clients) {
+					if (client !== ws) {
+						client.send(
+							JSON.stringify({
+								type: 'userJoined',
+								message: `A new user has joined! Total users: ${clients.size}`,
+								totalUsers: clients.size,
+							}),
+						)
+					}
+				}
+			},
+			message(ws: ServerWebSocket<ClientData>, message: string) {
+				// Log the message
+				console.log(`Message from ${ws.data.clientId}: ${message}`)
+
+				try {
+					// Parse the message
+					const parsedMessage = JSON.parse(message) as ParsedMessage
+
+					// Prepare response message
+					const responseMessage = {
+						type: 'message',
+						clientId: ws.data.clientId,
+						message: parsedMessage.text || message,
+						text: parsedMessage.text || message,
+						timestamp: new Date().toISOString(),
+					}
+
+					// Broadcast the message to all clients
+					for (const client of clients) {
+						client.send(JSON.stringify(responseMessage))
+					}
+				} catch (error) {
+					// If the message is not valid JSON, just broadcast it as is
+					console.error('Error parsing message:', error)
+
+					// Prepare response for plain text message
+					const responseMessage = {
+						type: 'message',
+						clientId: ws.data.clientId,
+						message: message,
+						text: message,
+						timestamp: new Date().toISOString(),
+					}
+
+					// Broadcast to all clients
+					for (const client of clients) {
+						client.send(JSON.stringify(responseMessage))
+					}
+				}
+			},
+			close(ws: ServerWebSocket<ClientData>) {
+				// Remove the client from the set of connected clients
+				clients.delete(ws)
+
+				// Log the disconnection
+				console.log(`Client disconnected: ${ws.data.clientId}`)
+
+				// Broadcast to all clients that a user has left
+				for (const client of clients) {
+					client.send(
+						JSON.stringify({
+							type: 'userLeft',
+							message: `A user has left. Total users: ${clients.size}`,
+							totalUsers: clients.size,
+						}),
+					)
+				}
+			},
+		},
+		port: process.env.PORT ? Number.parseInt(process.env.PORT) : 3001,
+		routes,
+		development: process.env.NODE_ENV !== 'production',
+	} as const
+
+	// Start the server
+	const server = serve(serverOptions)
+	console.log(`🚀 Server running at http://localhost:${server.port}/`)
+}
+
+// Start the server
+startServer().catch((error) => {
+	console.error('Failed to start server:', error)
+	process.exit(1)
+})

apps/kyc-server/src/utils/contentMap.tsx
@@ -1,0 +1,20 @@
+import React from 'react'
+import type { ReactNode } from 'react'
+import { About } from '../components/About'
+import { WebSocketDemo } from '../components/WebSocketDemo'
+
+// Centralized content map for both client and server
+export const contentMap: Record<string, ReactNode> = {
+	'/': 'Welcome to Kindfi KYC Server!',
+	'/react': 'This page is server-side rendered with React and Bun!',
+	'/websocket': <WebSocketDemo />,
+	'/about': <About />,
+}
+
+// Helper function to get content with a fallback
+export function getContent(
+	path: string,
+	fallback: ReactNode = 'Welcome to Kindfi KYC Server!',
+): ReactNode {
+	return contentMap[path] || fallback
+}