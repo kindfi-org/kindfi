df1ffe694b87352737bcb33282e9a1cb4e8ac98a
Ngozi Aguluka <melissacinta@gmail.com>
2025-03-25 10:45:51 +0100

    refactor project card to share common layout logic accross view modes

--- apps/web/components/shared/project-card.tsx
+++ apps/web/components/shared/project-card.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import type { Project, Tag } from '~/lib/types/projects.types'
+import { cn } from '~/lib/utils'
 import { getA11yColorMatch } from '~/lib/utils/color-utils'
 import { Progress } from '../base/progress'
 
@@ -61,60 +62,13 @@ function ProjectCard({ project, viewMode = 'grid' }: ProjectCardProps) {
 			? (project.current_amount / project.target_amount) * 100
 			: 0)
 
-	const cardContent =
-		viewMode === 'list' ? (
-			<div className="overflow-hidden transition-shadow duration-300 rounded-lg border border-gray-100 bg-white flex">
-				<div className="relative h-auto w-[200px] min-h-[180px]">
-					<Image
-						src={project.image_url || '/api/placeholder/400/320'}
-						alt={project.title}
-						fill
-						className="object-cover"
-					/>
-					<RenderCategories categories={project.categories || []} />
-				</div>
-
-				<div className="flex-1 p-5">
-					<h3 className="text-lg font-semibold mb-2">{project.title}</h3>
-					<p className="text-gray-600 mb-4 line-clamp-2 text-sm">
-						{project.description}
-					</p>
-
-					<ProjectDetails
-						project={project}
-						percentageComplete={percentageComplete}
-					/>
-				</div>
-			</div>
-		) : (
-			<div className="overflow-hidden transition-shadow duration-300 rounded-lg border border-gray-100 bg-white h-full relative">
-				<div className="relative w-full h-48">
-					<Image
-						src={project.image_url || '/api/placeholder/400/320'}
-						alt={project.title}
-						fill
-						className="object-cover transition-transform duration-500 hover:scale-105"
-					/>
-					<RenderCategories categories={project.categories || []} />
-				</div>
-
-				<div className="p-5">
-					<h3 className="text-lg font-semibold mb-2">{project.title}</h3>
-					<p className="text-gray-600 mb-4 line-clamp-2 text-sm">
-						{project.description}
-					</p>
-
-					<ProjectDetails
-						project={project}
-						percentageComplete={percentageComplete}
-					/>
-				</div>
-			</div>
-		)
-
 	return (
 		<Link href={`/project/${project.id}`} className="block h-full">
-			{cardContent}
+			<ProjectDetails
+				project={project}
+				percentageComplete={percentageComplete}
+				viewMode={viewMode}
+			/>
 		</Link>
 	)
 }
@@ -123,41 +77,71 @@ export default ProjectCard
 const ProjectDetails = ({
 	project,
 	percentageComplete,
+	viewMode,
 }: {
 	project: Project
+	viewMode: 'list' | 'grid'
 	percentageComplete: number
 }) => (
-	<>
-		<div className="mb-4">
-			<div className="flex justify-between text-sm mb-1">
-				<span className="font-semibold">
-					${project.current_amount?.toLocaleString() ?? '0'}
-				</span>
-				<span className="text-gray-500">
-					{percentageComplete.toFixed(2)}% of $
-					{project.target_amount?.toLocaleString()}
-				</span>
-			</div>
-			<Progress value={percentageComplete} className="h-2 bg-gray-100" />
+	<div
+		className={cn(
+			'overflow-hidden transition-shadow duration-300 rounded-lg border border-gray-100 bg-white',
+			viewMode === 'list' ? 'flex' : 'h-full relative',
+		)}
+	>
+		<div
+			className={cn(
+				viewMode === 'list'
+					? ' h-auto w-[200px] min-h-[180px]'
+					: ' w-full h-48',
+				'relative',
+			)}
+		>
+			<Image
+				src={project.image_url || '/api/placeholder/400/320'}
+				alt={project.title}
+				fill
+				className="object-cover transition-transform duration-500 hover:scale-105"
+			/>
+			<RenderCategories categories={project.categories || []} />
 		</div>
 
-		<div className="flex justify-between mb-4 text-center">
-			<div>
-				<p className="font-semibold">
-					${project.target_amount?.toLocaleString()}
-				</p>
-				<p className="text-xs text-gray-500">Goal</p>
+		<div className={'p-5 flex-1'}>
+			<h3 className="text-lg font-semibold mb-2">{project.title}</h3>
+			<p className="text-gray-600 mb-4 line-clamp-2 text-sm">
+				{project.description}
+			</p>
+			<div className="mb-4">
+				<div className="flex justify-between text-sm mb-1">
+					<span className="font-semibold">
+						${project.current_amount?.toLocaleString() ?? '0'}
+					</span>
+					<span className="text-gray-500">
+						{percentageComplete.toFixed(2)}% of $
+						{project.target_amount?.toLocaleString()}
+					</span>
+				</div>
+				<Progress value={percentageComplete} className="h-2 bg-gray-100" />
 			</div>
-			<div>
-				<p className="font-semibold">{project.investors_count ?? 0}</p>
-				<p className="text-xs text-gray-500">Investors</p>
-			</div>
-			<div>
-				<p className="font-semibold">${project.min_investment ?? 0}</p>
-				<p className="text-xs text-gray-500">Min. Investment</p>
-			</div>
-		</div>
 
-		<RenderTags tags={project.tags} />
-	</>
+			<div className="flex justify-between mb-4 text-center">
+				<div>
+					<p className="font-semibold">
+						${project.target_amount?.toLocaleString()}
+					</p>
+					<p className="text-xs text-gray-500">Goal</p>
+				</div>
+				<div>
+					<p className="font-semibold">{project.investors_count ?? 0}</p>
+					<p className="text-xs text-gray-500">Investors</p>
+				</div>
+				<div>
+					<p className="font-semibold">${project.min_investment ?? 0}</p>
+					<p className="text-xs text-gray-500">Min. Investment</p>
+				</div>
+			</div>
+
+			<RenderTags tags={project.tags} />
+		</div>
+	</div>
 )
