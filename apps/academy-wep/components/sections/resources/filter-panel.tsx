Wardlabz/wardfund

[academy] feat: create all resources section (#384)
* feat: create resource card component

* feat: create filter panel component

* feat: create all resources component

* feat: create resources page

* style: enhance responsive design

* chore: format code

* fix: resolve build errors

* feat: enhance codebase with various UI and logic updates

---------

Co-authored-by: Roberto Lucas <contact@andler.dev>

apps/academy-wep/components/sections/resources/filter-panel.tsx
@@ -1,0 +1,123 @@
+"use client"
+
+import { Button } from "~/components/base/button"
+import { Badge } from "~/components/base/badge"
+import { useResourceCounts } from "~/hooks/resources/use-resource-counts"
+import type { ResourceCategory, ResourceType, ExperienceLevel, Resource } from "~/lib/types/resources.types"
+import { cn } from "~/lib/utils"
+import { getTypeIcon } from "~/lib/utils/resources/get-type-icon"
+
+interface FilterPanelProps {
+  resources: Resource[]
+  selectedCategories: ResourceCategory[]
+  selectedTypes: ResourceType[]
+  selectedLevels: ExperienceLevel[]
+  onCategoryChange: (category: ResourceCategory) => void
+  onTypeChange: (type: ResourceType) => void
+  onLevelChange: (level: ExperienceLevel) => void
+  onResetFilters: () => void
+}
+
+const categories: ResourceCategory[] = ["Blockchain", "Stellar", "Web3", "WardFund", "Impact"]
+const types: ResourceType[] = ["article", "video", "guide", "document"]
+const levels: ExperienceLevel[] = ["Beginner", "Intermediate", "Advanced"]
+
+export function FilterPanel({
+  resources,
+  selectedCategories,
+  selectedTypes,
+  selectedLevels,
+  onCategoryChange,
+  onTypeChange,
+  onLevelChange,
+  onResetFilters,
+}: FilterPanelProps) {
+  const { categoryCounts, typeCounts, levelCounts } = useResourceCounts(resources)
+
+  return (
+    <div className="space-y-6">
+      {/* Categories */}
+      <div className="flex gap-2 items-center flex-wrap">
+        <h3 className="text-sm font-medium text-gray-700">Categories:</h3>
+        <div className="flex flex-wrap gap-2">
+          {categories.map((category) => (
+            <Badge
+              key={category}
+              variant={selectedCategories.includes(category) ? "default" : "outline"}
+              className={cn(
+                "cursor-pointer rounded-full text-xs px-3 py-1",
+                selectedCategories.includes(category)
+                  ? "bg-green-100 text-primary border-primary hover:bg-green-200"
+                  : "text-gray-700 hover:bg-gray-100"
+              )}
+              onClick={() => onCategoryChange(category)}
+            >
+              {category} ({categoryCounts[category]})
+            </Badge>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 lg:flex-row lg:items-start lg:justify-between">
+        {/* Resource Types */}
+        <div className="flex gap-2 items-center flex-wrap lg:flex-1">
+          <h3 className="text-sm font-medium text-gray-700 whitespace-nowrap">Types:</h3>
+          <div className="flex flex-wrap gap-2">
+            {types.map((type) => (
+              <Badge
+                key={type}
+                variant={selectedTypes.includes(type) ? "default" : "outline"}
+                className={cn(
+                  "cursor-pointer rounded-full text-xs px-3 py-1",
+                  selectedTypes.includes(type)
+                    ? "bg-green-100 text-primary hover:bg-green-200"
+                    : "text-gray-700 hover:bg-gray-100"
+                )}
+                onClick={() => onTypeChange(type)}
+              >
+                <div className="flex items-center gap-1">
+                  {getTypeIcon(type)}
+                  {type.charAt(0).toUpperCase() + type.slice(1)}s ({typeCounts[type]})
+                </div>
+              </Badge>
+            ))}
+          </div>
+        </div>
+
+        {/* Experience Level */}
+        <div className="flex gap-2 items-center flex-wrap lg:flex-1">
+          <h3 className="text-sm font-medium text-gray-700 whitespace-nowrap">Level:</h3>
+          <div className="flex flex-wrap gap-2">
+            {levels.map((level) => (
+              <Badge
+                key={level}
+                variant={selectedLevels.includes(level) ? "default" : "outline"}
+                className={cn(
+                  "cursor-pointer rounded-full text-xs px-3 py-1",
+                  selectedLevels.includes(level)
+                    ? "bg-green-100 text-primary hover:bg-green-200"
+                    : "text-gray-700 hover:bg-gray-100"
+                )}
+                onClick={() => onLevelChange(level)}
+              >
+                {level} ({levelCounts[level]})
+              </Badge>
+            ))}
+          </div>
+        </div>
+
+        {/* Reset Button */}
+        <div className="pt-2 lg:pt-0 lg:flex lg:items-start lg:justify-end lg:w-40">
+          <Button
+            variant="outline"
+            size="sm"
+            className="text-gray-800 bg-white hover:text-primary hover:bg-white border-gray-200 hover:border-primary rounded-full"
+            onClick={onResetFilters}
+          >
+            Reset Filters
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}

apps/academy-wep/lib/mock-data/resources.mock.ts
@@ -1,0 +1,132 @@
+import type { Resource } from "../types/resources.types";
+
+export const resources: Resource[] = [
+  {
+    id: "1",
+    type: "article",
+    category: "Blockchain",
+    title: "Understanding Blockchain",
+    description:
+      "A comprehensive guide to blockchain fundamentals and how they apply to modern financial systems and beyond.",
+    timeToConsume: 15,
+    level: "Beginner",
+    tags: ["Blockchain", "Fundamentals", "Technology"],
+    likes: 245,
+    comments: 45,
+    date: "2025-04-10",
+    slug: "understanding-blockchain",
+  },
+  {
+    id: "2",
+    type: "video",
+    category: "Stellar",
+    title: "Setting Up Your First Stellar Wallet",
+    description: "Step-by-step video tutorial on creating and securing your Stellar wallet for beginners.",
+    timeToConsume: 12,
+    level: "Beginner",
+    tags: ["Stellar", "Wallets", "Security"],
+    likes: 189,
+    comments: 32,
+    date: "2025-03-28",
+    slug: "setting-up-stellar-wallet",
+  },
+  {
+    id: "3",
+    type: "guide",
+    category: "Web3",
+    title: "Web3 Crowdfunding Best Practices",
+    description: "Learn the most effective strategies for running successful Web3 crowdfunding campaigns.",
+    timeToConsume: 20,
+    level: "Intermediate",
+    tags: ["Web3", "Crowdfunding", "Best Practices"],
+    likes: 312,
+    comments: 67,
+    date: "2025-04-02",
+    slug: "web3-crowdfunding-best-practices",
+  },
+  {
+    id: "4",
+    type: "guide",
+    category: "Stellar",
+    title: "Managing XLM & USDC Assets",
+    description: "A detailed guide on handling different assets on the Stellar network for your projects.",
+    timeToConsume: 25,
+    level: "Intermediate",
+    tags: ["Stellar", "Assets", "XLM", "USDC"],
+    likes: 132,
+    comments: 24,
+    date: "2025-03-15",
+    slug: "managing-xlm-usdc-assets",
+  },
+  {
+    id: "5",
+    type: "article",
+    category: "Blockchain",
+    title: "Transparency in Blockchain Donations",
+    description: "How blockchain technology ensures complete transparency in the donation process for nonprofits.",
+    timeToConsume: 10,
+    level: "Beginner",
+    tags: ["Blockchain", "Donations", "Transparency"],
+    likes: 178,
+    comments: 39,
+    date: "2025-03-22",
+    slug: "transparency-blockchain-donations",
+  },
+  {
+    id: "6",
+    type: "video",
+    category: "WardFund",
+    title: "WardFund Platform Tutorial",
+    description: "Complete walkthrough of the WardFund platform features and how to use them effectively.",
+    timeToConsume: 30,
+    level: "Beginner",
+    tags: ["WardFund", "Tutorial", "Platform"],
+    likes: 215,
+    comments: 53,
+    date: "2025-03-10",
+    slug: "wardfund-platform-tutorial",
+  },
+  {
+    id: "7",
+    type: "guide",
+    category: "Web3",
+    title: "Smart Contract Integration for Nonprofits",
+    description:
+      "Technical guide on implementing smart contracts for nonprofit organizations and social impact projects.",
+    timeToConsume: 35,
+    level: "Advanced",
+    tags: ["Web3", "Smart Contracts", "Nonprofits"],
+    likes: 156,
+    comments: 28,
+    date: "2025-02-28",
+    slug: "smart-contract-integration-nonprofits",
+  },
+  {
+    id: "8",
+    type: "document",
+    category: "Stellar",
+    title: "Stellar Blockchain Whitepaper",
+    description: "The official Stellar blockchain whitepaper with detailed technical specifications and use cases.",
+    timeToConsume: 45,
+    level: "Advanced",
+    tags: ["Stellar", "Whitepaper", "Technical"],
+    likes: 324,
+    comments: 47,
+    date: "2025-02-15",
+    slug: "stellar-blockchain-whitepaper",
+  },
+  {
+    id: "9",
+    type: "article",
+    category: "Impact",
+    title: "Impact Measurement on the Blockchain",
+    description: "How to track and measure social impact using blockchain technology.",
+    timeToConsume: 18,
+    level: "Intermediate",
+    tags: ["Impact", "Measurement", "Blockchain"],
+    likes: 198,
+    comments: 42,
+    date: "2025-03-05",
+    slug: "impact-measurement-blockchain",
+  },
+]

apps/academy-wep/lib/types/resources.types.ts
@@ -1,0 +1,19 @@
+export type ResourceType = "article" | "video" | "guide" | "document"
+export type ResourceCategory = "Blockchain" | "Stellar" | "Web3" | "WardFund" | "Impact"
+export type ExperienceLevel = "Beginner" | "Intermediate" | "Advanced"
+export type SortOption = "Most Recent" | "Oldest" | "Most Viewed"
+
+export interface Resource {
+  id: string
+  type: ResourceType
+  category: ResourceCategory
+  title: string
+  description: string
+  timeToConsume: number // in minutes
+  level: ExperienceLevel
+  tags: string[]
+  likes: number
+  comments: number
+  date: string
+  slug: string
+}

kindfi-org/kindfi

apps/academy-wep/components/sections/resources/filter-panel.tsx
@@ -1,0 +1,123 @@
+"use client"
+
+import { Button } from "~/components/base/button"
+import { Badge } from "~/components/base/badge"
+import { useResourceCounts } from "~/hooks/resources/use-resource-counts"
+import type { ResourceCategory, ResourceType, ExperienceLevel, Resource } from "~/lib/types/resources.types"
+import { cn } from "~/lib/utils"
+import { getTypeIcon } from "~/lib/utils/resources/get-type-icon"
+
+interface FilterPanelProps {
+  resources: Resource[]
+  selectedCategories: ResourceCategory[]
+  selectedTypes: ResourceType[]
+  selectedLevels: ExperienceLevel[]
+  onCategoryChange: (category: ResourceCategory) => void
+  onTypeChange: (type: ResourceType) => void
+  onLevelChange: (level: ExperienceLevel) => void
+  onResetFilters: () => void
+}
+
+const categories: ResourceCategory[] = ["Blockchain", "Stellar", "Web3", "KindFi", "Impact"]
+const types: ResourceType[] = ["article", "video", "guide", "document"]
+const levels: ExperienceLevel[] = ["Beginner", "Intermediate", "Advanced"]
+
+export function FilterPanel({
+  resources,
+  selectedCategories,
+  selectedTypes,
+  selectedLevels,
+  onCategoryChange,
+  onTypeChange,
+  onLevelChange,
+  onResetFilters,
+}: FilterPanelProps) {
+  const { categoryCounts, typeCounts, levelCounts } = useResourceCounts(resources)
+
+  return (
+    <div className="space-y-6">
+      {/* Categories */}
+      <div className="flex gap-2 items-center flex-wrap">
+        <h3 className="text-sm font-medium text-gray-700">Categories:</h3>
+        <div className="flex flex-wrap gap-2">
+          {categories.map((category) => (
+            <Badge
+              key={category}
+              variant={selectedCategories.includes(category) ? "default" : "outline"}
+              className={cn(
+                "cursor-pointer rounded-full text-xs px-3 py-1",
+                selectedCategories.includes(category)
+                  ? "bg-green-100 text-primary border-primary hover:bg-green-200"
+                  : "text-gray-700 hover:bg-gray-100"
+              )}
+              onClick={() => onCategoryChange(category)}
+            >
+              {category} ({categoryCounts[category]})
+            </Badge>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 lg:flex-row lg:items-start lg:justify-between">
+        {/* Resource Types */}
+        <div className="flex gap-2 items-center flex-wrap lg:flex-1">
+          <h3 className="text-sm font-medium text-gray-700 whitespace-nowrap">Types:</h3>
+          <div className="flex flex-wrap gap-2">
+            {types.map((type) => (
+              <Badge
+                key={type}
+                variant={selectedTypes.includes(type) ? "default" : "outline"}
+                className={cn(
+                  "cursor-pointer rounded-full text-xs px-3 py-1",
+                  selectedTypes.includes(type)
+                    ? "bg-green-100 text-primary hover:bg-green-200"
+                    : "text-gray-700 hover:bg-gray-100"
+                )}
+                onClick={() => onTypeChange(type)}
+              >
+                <div className="flex items-center gap-1">
+                  {getTypeIcon(type)}
+                  {type.charAt(0).toUpperCase() + type.slice(1)}s ({typeCounts[type]})
+                </div>
+              </Badge>
+            ))}
+          </div>
+        </div>
+
+        {/* Experience Level */}
+        <div className="flex gap-2 items-center flex-wrap lg:flex-1">
+          <h3 className="text-sm font-medium text-gray-700 whitespace-nowrap">Level:</h3>
+          <div className="flex flex-wrap gap-2">
+            {levels.map((level) => (
+              <Badge
+                key={level}
+                variant={selectedLevels.includes(level) ? "default" : "outline"}
+                className={cn(
+                  "cursor-pointer rounded-full text-xs px-3 py-1",
+                  selectedLevels.includes(level)
+                    ? "bg-green-100 text-primary hover:bg-green-200"
+                    : "text-gray-700 hover:bg-gray-100"
+                )}
+                onClick={() => onLevelChange(level)}
+              >
+                {level} ({levelCounts[level]})
+              </Badge>
+            ))}
+          </div>
+        </div>
+
+        {/* Reset Button */}
+        <div className="pt-2 lg:pt-0 lg:flex lg:items-start lg:justify-end lg:w-40">
+          <Button
+            variant="outline"
+            size="sm"
+            className="text-gray-800 bg-white hover:text-primary hover:bg-white border-gray-200 hover:border-primary rounded-full"
+            onClick={onResetFilters}
+          >
+            Reset Filters
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}

apps/academy-wep/lib/mock-data/resources.mock.ts
@@ -1,0 +1,132 @@
+import type { Resource } from "../types/resources.types";
+
+export const resources: Resource[] = [
+  {
+    id: "1",
+    type: "article",
+    category: "Blockchain",
+    title: "Understanding Blockchain",
+    description:
+      "A comprehensive guide to blockchain fundamentals and how they apply to modern financial systems and beyond.",
+    timeToConsume: 15,
+    level: "Beginner",
+    tags: ["Blockchain", "Fundamentals", "Technology"],
+    likes: 245,
+    comments: 45,
+    date: "2025-04-10",
+    slug: "understanding-blockchain",
+  },
+  {
+    id: "2",
+    type: "video",
+    category: "Stellar",
+    title: "Setting Up Your First Stellar Wallet",
+    description: "Step-by-step video tutorial on creating and securing your Stellar wallet for beginners.",
+    timeToConsume: 12,
+    level: "Beginner",
+    tags: ["Stellar", "Wallets", "Security"],
+    likes: 189,
+    comments: 32,
+    date: "2025-03-28",
+    slug: "setting-up-stellar-wallet",
+  },
+  {
+    id: "3",
+    type: "guide",
+    category: "Web3",
+    title: "Web3 Crowdfunding Best Practices",
+    description: "Learn the most effective strategies for running successful Web3 crowdfunding campaigns.",
+    timeToConsume: 20,
+    level: "Intermediate",
+    tags: ["Web3", "Crowdfunding", "Best Practices"],
+    likes: 312,
+    comments: 67,
+    date: "2025-04-02",
+    slug: "web3-crowdfunding-best-practices",
+  },
+  {
+    id: "4",
+    type: "guide",
+    category: "Stellar",
+    title: "Managing XLM & USDC Assets",
+    description: "A detailed guide on handling different assets on the Stellar network for your projects.",
+    timeToConsume: 25,
+    level: "Intermediate",
+    tags: ["Stellar", "Assets", "XLM", "USDC"],
+    likes: 132,
+    comments: 24,
+    date: "2025-03-15",
+    slug: "managing-xlm-usdc-assets",
+  },
+  {
+    id: "5",
+    type: "article",
+    category: "Blockchain",
+    title: "Transparency in Blockchain Donations",
+    description: "How blockchain technology ensures complete transparency in the donation process for nonprofits.",
+    timeToConsume: 10,
+    level: "Beginner",
+    tags: ["Blockchain", "Donations", "Transparency"],
+    likes: 178,
+    comments: 39,
+    date: "2025-03-22",
+    slug: "transparency-blockchain-donations",
+  },
+  {
+    id: "6",
+    type: "video",
+    category: "KindFi",
+    title: "KindFi Platform Tutorial",
+    description: "Complete walkthrough of the KindFi platform features and how to use them effectively.",
+    timeToConsume: 30,
+    level: "Beginner",
+    tags: ["KindFi", "Tutorial", "Platform"],
+    likes: 215,
+    comments: 53,
+    date: "2025-03-10",
+    slug: "kindfi-platform-tutorial",
+  },
+  {
+    id: "7",
+    type: "guide",
+    category: "Web3",
+    title: "Smart Contract Integration for Nonprofits",
+    description:
+      "Technical guide on implementing smart contracts for nonprofit organizations and social impact projects.",
+    timeToConsume: 35,
+    level: "Advanced",
+    tags: ["Web3", "Smart Contracts", "Nonprofits"],
+    likes: 156,
+    comments: 28,
+    date: "2025-02-28",
+    slug: "smart-contract-integration-nonprofits",
+  },
+  {
+    id: "8",
+    type: "document",
+    category: "Stellar",
+    title: "Stellar Blockchain Whitepaper",
+    description: "The official Stellar blockchain whitepaper with detailed technical specifications and use cases.",
+    timeToConsume: 45,
+    level: "Advanced",
+    tags: ["Stellar", "Whitepaper", "Technical"],
+    likes: 324,
+    comments: 47,
+    date: "2025-02-15",
+    slug: "stellar-blockchain-whitepaper",
+  },
+  {
+    id: "9",
+    type: "article",
+    category: "Impact",
+    title: "Impact Measurement on the Blockchain",
+    description: "How to track and measure social impact using blockchain technology.",
+    timeToConsume: 18,
+    level: "Intermediate",
+    tags: ["Impact", "Measurement", "Blockchain"],
+    likes: 198,
+    comments: 42,
+    date: "2025-03-05",
+    slug: "impact-measurement-blockchain",
+  },
+]

apps/academy-wep/lib/types/resources.types.ts
@@ -1,0 +1,19 @@
+export type ResourceType = "article" | "video" | "guide" | "document"
+export type ResourceCategory = "Blockchain" | "Stellar" | "Web3" | "KindFi" | "Impact"
+export type ExperienceLevel = "Beginner" | "Intermediate" | "Advanced"
+export type SortOption = "Most Recent" | "Oldest" | "Most Viewed"
+
+export interface Resource {
+  id: string
+  type: ResourceType
+  category: ResourceCategory
+  title: string
+  description: string
+  timeToConsume: number // in minutes
+  level: ExperienceLevel
+  tags: string[]
+  likes: number
+  comments: number
+  date: string
+  slug: string
+}