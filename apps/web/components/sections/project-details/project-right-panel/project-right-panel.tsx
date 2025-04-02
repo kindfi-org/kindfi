a84f986b600f430512a5cf0ac4feba951fd794e3
Luis Carrión <louiss862@gmail.com>
2025-04-02 19:01:33 -0300

    feat(web): add component project-right-panel

---
+++ apps/web/components/sections/project-details/project-right-panel/project-right-panel.tsx
@@ -0,0 +1,78 @@
+'use client'
+import { useEffect, useState } from 'react'
+import type { viewModeProps } from './type'
+import { ViewClosed } from './view-closed'
+import { ViewDonated } from './view-donated'
+import { type FormValue, ViewInitial } from './view-initial'
+
+export function ProjectRightPanel() {
+	const [amountOfSupport, setAmountOfSupport] = useState(
+		projectData.raisedAmount,
+	)
+	const [viewMode, setViewMode] = useState<viewModeProps>('initial')
+	const percentage = Math.round(
+		(amountOfSupport / projectData.goalAmount) * 100,
+	)
+
+	const onSubmit = (data: FormValue) => {
+		setViewMode('donated')
+		setAmountOfSupport((stateOld) => stateOld + Number(data.amountOfSupport))
+	}
+
+	useEffect(() => {
+		let timeoutId: ReturnType<typeof setTimeout>
+		if (viewMode === 'donated') {
+			timeoutId = setTimeout(() => setViewMode('closed'), 4000)
+		}
+		return () => clearTimeout(timeoutId)
+	}, [viewMode])
+
+	const renderView = () => {
+		switch (viewMode) {
+			case 'donated':
+				return (
+					<ViewDonated
+						amountOfSupport={amountOfSupport}
+						goal={projectData.goalAmount}
+						percentage={percentage}
+					/>
+				)
+			case 'closed':
+				return <ViewClosed changeViewMode={() => setViewMode('initial')} />
+			default:
+				return (
+					<ViewInitial
+						amountOfSupport={amountOfSupport}
+						goal={projectData.goalAmount}
+						percentage={percentage}
+						onSubmit={onSubmit}
+					/>
+				)
+		}
+	}
+
+	return <div className="max-w-[480px]">{renderView()}</div>
+}
+
+const projectData = {
+	title: 'Energy storage with zero lithium and zero-degradation',
+	raisedAmount: 963419,
+	goalAmount: 1500000,
+	nftTiers: [
+		{
+			title: 'Early Bird NFT',
+			description: 'Support with $100',
+			rightSite: '42 left',
+		},
+		{
+			title: 'Impact Maker NFT',
+			description: 'Support with $250',
+			rightSite: '18 left',
+		},
+		{
+			title: 'Founding Supporter NFT',
+			description: 'Support with $500',
+			rightSite: '5 left',
+		},
+	],
+}
