b7bcce8d3a5a8deb23cd305a3221f897dc96c092
Roberto Lucas <contact@andler.dev>
2025-04-28 14:32:12 -0600

    chore: mobile-app format lintin  (#472)
    * chore(style): format & lint mobile-app
    
    * chore(style): handle ts errors

--- apps/mobile/mobile-app/components/ui/divider/index.tsx
+++ apps/mobile/mobile-app/components/ui/divider/index.tsx
@@ -1,40 +1,40 @@
-'use client';
-import React from 'react';
-import { tva } from '@gluestack-ui/nativewind-utils/tva';
-import { Platform, View } from 'react-native';
-import type { VariantProps } from '@gluestack-ui/nativewind-utils';
+'use client'
+import type { VariantProps } from '@gluestack-ui/nativewind-utils'
+import { tva } from '@gluestack-ui/nativewind-utils/tva'
+import React from 'react'
+import { Platform, View } from 'react-native'
 
 const dividerStyle = tva({
-  base: 'bg-background-200',
-  variants: {
-    orientation: {
-      vertical: 'w-px h-full',
-      horizontal: 'h-px w-full',
-    },
-  },
-});
+	base: 'bg-background-200',
+	variants: {
+		orientation: {
+			vertical: 'w-px h-full',
+			horizontal: 'h-px w-full',
+		},
+	},
+})
 
 type IUIDividerProps = React.ComponentPropsWithoutRef<typeof View> &
-  VariantProps<typeof dividerStyle>;
+	VariantProps<typeof dividerStyle>
 
 const Divider = React.forwardRef<
-  React.ElementRef<typeof View>,
-  IUIDividerProps
+	React.ElementRef<typeof View>,
+	IUIDividerProps
 >(({ className, orientation = 'horizontal', ...props }, ref) => {
-  return (
-    <View
-      ref={ref}
-      {...props}
-      aria-orientation={orientation}
-      role={Platform.OS === 'web' ? 'separator' : undefined}
-      className={dividerStyle({
-        orientation,
-        class: className,
-      })}
-    />
-  );
-});
+	return (
+		<View
+			ref={ref}
+			{...props}
+			aria-orientation={orientation}
+			role={Platform.OS === 'web' ? 'separator' : undefined}
+			className={dividerStyle({
+				orientation,
+				class: className,
+			})}
+		/>
+	)
+})
 
-Divider.displayName = 'Divider';
+Divider.displayName = 'Divider'
 
-export { Divider };
+export { Divider }
