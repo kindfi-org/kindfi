737829c1d1d4577e35ab42ebad37caddd48ce5e1
bran18 <andreyfdez18@gmail.com>
2025-04-29 12:13:25 -0600

    fix: restore application

--- apps/mobile/mobile-app/app/index.tsx
+++ apps/mobile/mobile-app/app/index.tsx
@@ -1,50 +1,86 @@
-import ImpactSection from '@/components/impact/impact-section'
-import KindFiOnboarding from '@/components/onboarding/Onboarding'
-import { Text } from '@/components/ui/text'
-import React, { useState } from 'react'
-import { ScrollView, StatusBar, TouchableOpacity, View } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+/* eslint-disable import/no-unresolved */
+import React from "react";
+import Gradient from "@/assets/Icons/Gradient";
+import DocumentData from "@/assets/Icons/DocumentData";
+import LightBulbPerson from "@/assets/Icons/LightbulbPerson";
+import Rocket from "@/assets/Icons/Rocket";
+import Logo from "@/assets/Icons/Logo";
+import { Box } from "@/components/ui/box";
+import { ScrollView } from "react-native";
+import { Text } from "@/components/ui/text";
+
+import { Link } from "expo-router";
+
+const FeatureCard = ({ iconSvg: IconSvg, name, desc }: any) => {
+  return (
+    <Box
+      className="p-4 m-2 border rounded flex-column border-w-1 border-outline-700 md:flex-1"
+      key={name}
+    >
+      <Box className="flex flex-row items-center">
+        <Text>
+          <IconSvg />
+        </Text>
+        <Text className="ml-2 text-xl font-medium text-typography-white">
+          {name}
+        </Text>
+      </Box>
+      <Text className="mt-2 text-typography-400">{desc}</Text>
+    </Box>
+  );
+};
 
 export default function Home() {
-	const insets = useSafeAreaInsets()
-	const [showOnboarding, setShowOnboarding] = useState(true)
+  return (
+    <Box className="flex-1 bg-black h-[100vh]">
+      <ScrollView
+        style={{ height: "100%" }}
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <Box className="absolute h-[500px] w-[500px] lg:w-[700px] lg:h-[700px]">
+          <Gradient />
+        </Box>
+        <Box className="flex items-center flex-1 mx-5 my-16 lg:my-24 lg:mx-32">
+          <Box className="gap-10 base:flex-col sm:flex-row justify-between sm:w-[80%] md:flex-1">
+            <Box className="items-center px-6 py-2 rounded-full bg-background-template flex-column md:flex-row md:self-start">
+              <Text className="font-normal text-typography-white">
+                Get started by editing
+              </Text>
+              <Text className="ml-2 font-medium text-typography-white">
+                ./App.tsx
+              </Text>
+            </Box>
+            <Link href="/tabs/">
+              <Box className="items-center px-6 py-2 rounded-full bg-background-template flex-column sm:flex-row md:self-start">
+                <Text className="font-normal text-typography-white">
+                  Explore Tab Navigation
+                </Text>
+              </Box>
+            </Link>
+          </Box>
+          <Box className="flex-1 justify-center items-center h-[20px] w-[300px] lg:h-[160px] lg:w-[400px]">
+            <Logo />
+          </Box>
 
-	return (
-		<View className="flex-1 bg-background-light">
-			<StatusBar
-				barStyle="dark-content"
-				backgroundColor="transparent"
-				translucent={false}
-			/>
-
-			{/* Onboarding Modal */}
-			<KindFiOnboarding
-				isOpen={showOnboarding}
-				onClose={() => setShowOnboarding(false)}
-			/>
-
-			<ScrollView
-				style={{ height: '100%' }}
-				contentContainerStyle={{
-					flexGrow: 1,
-					paddingTop: insets.top,
-					paddingBottom: insets.bottom,
-					paddingLeft: insets.left,
-					paddingRight: insets.right,
-				}}
-			>
-				<ImpactSection />
-
-				{/* Button to open modal */}
-				<TouchableOpacity
-					onPress={() => setShowOnboarding(true)}
-					className="m-6 py-3 px-6 gradient-btn rounded-lg"
-				>
-					<Text className="text-white font-medium text-center">
-						Show Onboarding
-					</Text>
-				</TouchableOpacity>
-			</ScrollView>
-		</View>
-	)
+          <Box className="flex-column md:flex-row">
+            <FeatureCard
+              iconSvg={DocumentData}
+              name="Docs"
+              desc="Find in-depth information about gluestack features and API."
+            />
+            <FeatureCard
+              iconSvg={LightBulbPerson}
+              name="Learn"
+              desc="Learn about gluestack in an interactive course with quizzes!"
+            />
+            <FeatureCard
+              iconSvg={Rocket}
+              name="Deploy"
+              desc="Instantly drop your gluestack site to a shareable URL with vercel."
+            />
+          </Box>
+        </Box>
+      </ScrollView>
+    </Box>
+  );
 }
