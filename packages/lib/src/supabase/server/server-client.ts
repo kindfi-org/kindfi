d4ebc881d67c7ff2c391fa56544f54e5cc40a5f7
Derian <59376626+derianrddev@users.noreply.github.com>
2025-04-30 15:51:56 -0600

    [lib] feat: create reusable TanStack Query Hook and server-side query helpers (#487)
    * feat: add reusable Supabase clients, server-side query helpers and React Query hook
    
    * refactor: unify Supabase client types using central TypedSupabaseClient
    
    * chore: add index exports and configure tsconfig paths for cleaner imports
    
    * chore: refactor: update Supabase client import
    
    ---------
    
    Co-authored-by: Brandon Fernández <31634868+Bran18@users.noreply.github.com>

---
+++ packages/lib/src/supabase/server/server-client.ts
@@ -0,0 +1,43 @@
+import type { Database } from '@services/supabase'
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import type { TypedSupabaseClient } from '../../types/supabase-client.type'
+
+/**
+ * Creates a new Supabase server-side client using cookies for authentication.
+ *
+ * Intended for Server Components and API routes.
+ *
+ * @returns A Supabase client bound to the current user's session.
+ *
+ * @example
+ * const supabase = await createSupabaseServerClient();
+ * const { data } = await supabase.from('projects').select('*');
+ */
+export async function createSupabaseServerClient(): Promise<TypedSupabaseClient> {
+	const cookieStore = await cookies()
+
+	return createServerClient<Database>(
+		process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll()
+				},
+				setAll(cookiesToSet) {
+					try {
+						// biome-ignore lint/complexity/noForEach: <explanation>
+						cookiesToSet.forEach(({ name, value, options }) => {
+							cookieStore.set(name, value, options)
+						})
+					} catch (error) {
+						// The `set` method was called from a Server Component.
+						// This can be ignored if you have middleware refreshing
+						// user sessions.
+					}
+				},
+			},
+		},
+	)
+}
