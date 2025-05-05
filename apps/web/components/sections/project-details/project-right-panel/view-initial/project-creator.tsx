Wardlabz/wardfund

Feat project right panel (#393)
* feat(web): add new badge variant

* feat(web): add new button variant

* feat(web): add new card variant

* feat(web): add common components

* feat(web): add components to viewInitial

* feat(web): add components to viewDonated

* feat(web): add components to viewClosed

* feat(web): add component project-right-panel

* chore(web): remove unnecessary files

* chore(web): update radix-progress package

* refactor(web): changes according to codeRabbit

* chore(web): add mock-data

* refactor(web): remove data from components and use mock-data

---------

Co-authored-by: Roberto Lucas <andler.dev@gmail.com>
Co-authored-by: Brandon Fernández <31634868+Bran18@users.noreply.github.com>

apps/web/components/sections/project-details/project-right-panel/view-initial/project-creator.tsx
@@ -1,0 +1,47 @@
+import { Check, CircleUserRound } from 'lucide-react'
+import { Button } from '~/components/base/button'
+import { PrimaryCard } from '~/components/cards/primary-card'
+import { TitleCardDetail } from '../title-card-detail'
+
+type ProjectCreatorProps = {
+	creatorName: string
+	joinDate: string
+	isVerified?: boolean
+}
+
+export function ProjectCreator({
+	creatorName,
+	joinDate,
+	isVerified = true,
+}: ProjectCreatorProps) {
+	return (
+		<PrimaryCard className="space-y-4">
+			<TitleCardDetail>Project Creator</TitleCardDetail>
+
+			<div className="flex items-center gap-4">
+				<CircleUserRound className="size-10" />
+				<div>
+					<span className="font-semibold block text-gray-700">
+						{creatorName}
+					</span>
+					<span className="text-gray-600">{joinDate}</span>
+				</div>
+			</div>
+			<Button variant="outline" size="wide">
+				Contact Creator
+			</Button>
+
+			{isVerified && (
+				<div className="flex items-center gap-4 rounded-lg bg-lime-200/85 p-4">
+					<Check className="text-green-600" />
+					<div>
+						<span className="font-semibold block">Verified Project</span>
+						<span className="text-gray-600">
+							This project has been verified by WardFund
+						</span>
+					</div>
+				</div>
+			)}
+		</PrimaryCard>
+	)
+}

kindfi-org/kindfi

apps/web/components/sections/project-details/project-right-panel/view-initial/project-creator.tsx
@@ -1,0 +1,47 @@
+import { Check, CircleUserRound } from 'lucide-react'
+import { Button } from '~/components/base/button'
+import { PrimaryCard } from '~/components/cards/primary-card'
+import { TitleCardDetail } from '../title-card-detail'
+
+type ProjectCreatorProps = {
+	creatorName: string
+	joinDate: string
+	isVerified?: boolean
+}
+
+export function ProjectCreator({
+	creatorName,
+	joinDate,
+	isVerified = true,
+}: ProjectCreatorProps) {
+	return (
+		<PrimaryCard className="space-y-4">
+			<TitleCardDetail>Project Creator</TitleCardDetail>
+
+			<div className="flex items-center gap-4">
+				<CircleUserRound className="size-10" />
+				<div>
+					<span className="font-semibold block text-gray-700">
+						{creatorName}
+					</span>
+					<span className="text-gray-600">{joinDate}</span>
+				</div>
+			</div>
+			<Button variant="outline" size="wide">
+				Contact Creator
+			</Button>
+
+			{isVerified && (
+				<div className="flex items-center gap-4 rounded-lg bg-lime-200/85 p-4">
+					<Check className="text-green-600" />
+					<div>
+						<span className="font-semibold block">Verified Project</span>
+						<span className="text-gray-600">
+							This project has been verified by KindFi
+						</span>
+					</div>
+				</div>
+			)}
+		</PrimaryCard>
+	)
+}