Wardlabz/wardfund

Create Main Components - Learning Paths Section #305 (#458)
* Create Main Components - Learning Paths Section #305

* chore: fix build issue

---------

Co-authored-by: Brandon Fernández <31634868+Bran18@users.noreply.github.com>
Co-authored-by: bran18 <andreyfdez18@gmail.com>

apps/academy-wep/app/learning-paths/page.tsx
@@ -1,0 +1,16 @@
+'use client'
+
+import LearningPaths from '~/components/learningpaths/LearningPaths'
+
+export default function LearnPage() {
+	return (
+		<>
+			<title>Learning Paths | WardFund Academy</title>
+			<meta
+				name="description"
+				content="Explore educational modules and resources to enhance your financial literacy"
+			/>
+			<LearningPaths />
+		</>
+	)
+}

kindfi-org/kindfi

apps/academy-wep/app/learning-paths/page.tsx
@@ -1,0 +1,16 @@
+'use client'
+
+import LearningPaths from '~/components/learningpaths/LearningPaths'
+
+export default function LearnPage() {
+	return (
+		<>
+			<title>Learning Paths | KindFi Academy</title>
+			<meta
+				name="description"
+				content="Explore educational modules and resources to enhance your financial literacy"
+			/>
+			<LearningPaths />
+		</>
+	)
+}