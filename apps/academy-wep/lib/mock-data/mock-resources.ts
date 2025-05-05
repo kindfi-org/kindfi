Wardlabz/wardfund

[academy] feat: create lessons view (#467)
* feat: add breadcrumbs to lesson page

* feat: add lesson header to lesson page

* feat: add lesson content tab to lesson page

* feat: add knowledge check tab to lesson page

* feat: add lesson navigation component to lesson page

* refactor: improve lesson navigation and progress locking behavior

* refactor: improve progress display in header and navigation

* feat: unlock lessons after quiz completion

* feat: add dynamic correction and feedback for quiz answers

---------

Co-authored-by: Brandon Fernández <31634868+Bran18@users.noreply.github.com>

apps/academy-wep/lib/mock-data/mock-resources.ts
@@ -1,0 +1,139 @@
+import type { Resource } from '../types/resources.types'
+
+export const resources: Resource[] = [
+	{
+		id: '1',
+		type: 'article',
+		category: 'Blockchain',
+		title: 'Understanding Blockchain',
+		description:
+			'A comprehensive guide to blockchain fundamentals and how they apply to modern financial systems and beyond.',
+		timeToConsume: 15,
+		level: 'Beginner',
+		tags: ['Blockchain', 'Fundamentals', 'Technology'],
+		likes: 245,
+		comments: 45,
+		date: '2025-04-10',
+		slug: 'understanding-blockchain',
+	},
+	{
+		id: '2',
+		type: 'video',
+		category: 'Stellar',
+		title: 'Setting Up Your First Stellar Wallet',
+		description:
+			'Step-by-step video tutorial on creating and securing your Stellar wallet for beginners.',
+		timeToConsume: 12,
+		level: 'Beginner',
+		tags: ['Stellar', 'Wallets', 'Security'],
+		likes: 189,
+		comments: 32,
+		date: '2025-03-28',
+		slug: 'setting-up-stellar-wallet',
+	},
+	{
+		id: '3',
+		type: 'guide',
+		category: 'Web3',
+		title: 'Web3 Crowdfunding Best Practices',
+		description:
+			'Learn the most effective strategies for running successful Web3 crowdfunding campaigns.',
+		timeToConsume: 20,
+		level: 'Intermediate',
+		tags: ['Web3', 'Crowdfunding', 'Best Practices'],
+		likes: 312,
+		comments: 67,
+		date: '2025-04-02',
+		slug: 'web3-crowdfunding-best-practices',
+	},
+	{
+		id: '4',
+		type: 'guide',
+		category: 'Stellar',
+		title: 'Managing XLM & USDC Assets',
+		description:
+			'A detailed guide on handling different assets on the Stellar network for your projects.',
+		timeToConsume: 25,
+		level: 'Intermediate',
+		tags: ['Stellar', 'Assets', 'XLM', 'USDC'],
+		likes: 132,
+		comments: 24,
+		date: '2025-03-15',
+		slug: 'managing-xlm-usdc-assets',
+	},
+	{
+		id: '5',
+		type: 'article',
+		category: 'Blockchain',
+		title: 'Transparency in Blockchain Donations',
+		description:
+			'How blockchain technology ensures complete transparency in the donation process for nonprofits.',
+		timeToConsume: 10,
+		level: 'Beginner',
+		tags: ['Blockchain', 'Donations', 'Transparency'],
+		likes: 178,
+		comments: 39,
+		date: '2025-03-22',
+		slug: 'transparency-blockchain-donations',
+	},
+	{
+		id: '6',
+		type: 'video',
+		category: 'WardFund',
+		title: 'WardFund Platform Tutorial',
+		description:
+			'Complete walkthrough of the WardFund platform features and how to use them effectively.',
+		timeToConsume: 30,
+		level: 'Beginner',
+		tags: ['WardFund', 'Tutorial', 'Platform'],
+		likes: 215,
+		comments: 53,
+		date: '2025-03-10',
+		slug: 'wardfund-platform-tutorial',
+	},
+	{
+		id: '7',
+		type: 'guide',
+		category: 'Web3',
+		title: 'Smart Contract Integration for Nonprofits',
+		description:
+			'Technical guide on implementing smart contracts for nonprofit organizations and social impact projects.',
+		timeToConsume: 35,
+		level: 'Advanced',
+		tags: ['Web3', 'Smart Contracts', 'Nonprofits'],
+		likes: 156,
+		comments: 28,
+		date: '2025-02-28',
+		slug: 'smart-contract-integration-nonprofits',
+	},
+	{
+		id: '8',
+		type: 'document',
+		category: 'Stellar',
+		title: 'Stellar Blockchain Whitepaper',
+		description:
+			'The official Stellar blockchain whitepaper with detailed technical specifications and use cases.',
+		timeToConsume: 45,
+		level: 'Advanced',
+		tags: ['Stellar', 'Whitepaper', 'Technical'],
+		likes: 324,
+		comments: 47,
+		date: '2025-02-15',
+		slug: 'stellar-blockchain-whitepaper',
+	},
+	{
+		id: '9',
+		type: 'article',
+		category: 'Impact',
+		title: 'Impact Measurement on the Blockchain',
+		description:
+			'How to track and measure social impact using blockchain technology.',
+		timeToConsume: 18,
+		level: 'Intermediate',
+		tags: ['Impact', 'Measurement', 'Blockchain'],
+		likes: 198,
+		comments: 42,
+		date: '2025-03-05',
+		slug: 'impact-measurement-blockchain',
+	},
+]

kindfi-org/kindfi

apps/academy-wep/lib/mock-data/mock-resources.ts
@@ -1,0 +1,139 @@
+import type { Resource } from '../types/resources.types'
+
+export const resources: Resource[] = [
+	{
+		id: '1',
+		type: 'article',
+		category: 'Blockchain',
+		title: 'Understanding Blockchain',
+		description:
+			'A comprehensive guide to blockchain fundamentals and how they apply to modern financial systems and beyond.',
+		timeToConsume: 15,
+		level: 'Beginner',
+		tags: ['Blockchain', 'Fundamentals', 'Technology'],
+		likes: 245,
+		comments: 45,
+		date: '2025-04-10',
+		slug: 'understanding-blockchain',
+	},
+	{
+		id: '2',
+		type: 'video',
+		category: 'Stellar',
+		title: 'Setting Up Your First Stellar Wallet',
+		description:
+			'Step-by-step video tutorial on creating and securing your Stellar wallet for beginners.',
+		timeToConsume: 12,
+		level: 'Beginner',
+		tags: ['Stellar', 'Wallets', 'Security'],
+		likes: 189,
+		comments: 32,
+		date: '2025-03-28',
+		slug: 'setting-up-stellar-wallet',
+	},
+	{
+		id: '3',
+		type: 'guide',
+		category: 'Web3',
+		title: 'Web3 Crowdfunding Best Practices',
+		description:
+			'Learn the most effective strategies for running successful Web3 crowdfunding campaigns.',
+		timeToConsume: 20,
+		level: 'Intermediate',
+		tags: ['Web3', 'Crowdfunding', 'Best Practices'],
+		likes: 312,
+		comments: 67,
+		date: '2025-04-02',
+		slug: 'web3-crowdfunding-best-practices',
+	},
+	{
+		id: '4',
+		type: 'guide',
+		category: 'Stellar',
+		title: 'Managing XLM & USDC Assets',
+		description:
+			'A detailed guide on handling different assets on the Stellar network for your projects.',
+		timeToConsume: 25,
+		level: 'Intermediate',
+		tags: ['Stellar', 'Assets', 'XLM', 'USDC'],
+		likes: 132,
+		comments: 24,
+		date: '2025-03-15',
+		slug: 'managing-xlm-usdc-assets',
+	},
+	{
+		id: '5',
+		type: 'article',
+		category: 'Blockchain',
+		title: 'Transparency in Blockchain Donations',
+		description:
+			'How blockchain technology ensures complete transparency in the donation process for nonprofits.',
+		timeToConsume: 10,
+		level: 'Beginner',
+		tags: ['Blockchain', 'Donations', 'Transparency'],
+		likes: 178,
+		comments: 39,
+		date: '2025-03-22',
+		slug: 'transparency-blockchain-donations',
+	},
+	{
+		id: '6',
+		type: 'video',
+		category: 'KindFi',
+		title: 'KindFi Platform Tutorial',
+		description:
+			'Complete walkthrough of the KindFi platform features and how to use them effectively.',
+		timeToConsume: 30,
+		level: 'Beginner',
+		tags: ['KindFi', 'Tutorial', 'Platform'],
+		likes: 215,
+		comments: 53,
+		date: '2025-03-10',
+		slug: 'kindfi-platform-tutorial',
+	},
+	{
+		id: '7',
+		type: 'guide',
+		category: 'Web3',
+		title: 'Smart Contract Integration for Nonprofits',
+		description:
+			'Technical guide on implementing smart contracts for nonprofit organizations and social impact projects.',
+		timeToConsume: 35,
+		level: 'Advanced',
+		tags: ['Web3', 'Smart Contracts', 'Nonprofits'],
+		likes: 156,
+		comments: 28,
+		date: '2025-02-28',
+		slug: 'smart-contract-integration-nonprofits',
+	},
+	{
+		id: '8',
+		type: 'document',
+		category: 'Stellar',
+		title: 'Stellar Blockchain Whitepaper',
+		description:
+			'The official Stellar blockchain whitepaper with detailed technical specifications and use cases.',
+		timeToConsume: 45,
+		level: 'Advanced',
+		tags: ['Stellar', 'Whitepaper', 'Technical'],
+		likes: 324,
+		comments: 47,
+		date: '2025-02-15',
+		slug: 'stellar-blockchain-whitepaper',
+	},
+	{
+		id: '9',
+		type: 'article',
+		category: 'Impact',
+		title: 'Impact Measurement on the Blockchain',
+		description:
+			'How to track and measure social impact using blockchain technology.',
+		timeToConsume: 18,
+		level: 'Intermediate',
+		tags: ['Impact', 'Measurement', 'Blockchain'],
+		likes: 198,
+		comments: 42,
+		date: '2025-03-05',
+		slug: 'impact-measurement-blockchain',
+	},
+]