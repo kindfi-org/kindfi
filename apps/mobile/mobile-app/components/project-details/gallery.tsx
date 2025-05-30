ec3c464f30cc05b3d762184b48b2a0a67c596b8b
Lazarus <alvarolazarus@gmail.com>
2025-05-29 23:46:00 -0600

    fix: fixed home screen errors

--- apps/mobile/mobile-app/components/project-details/gallery.tsx
+++ apps/mobile/mobile-app/components/project-details/gallery.tsx
@@ -1,10 +1,13 @@
 import { Image, View } from 'react-native'
-import education from '../../assets/images/education.jpg'
 
 export default function Gallery() {
 	return (
 		<View className="h-[30vh] w-full items-center justify-center">
-			<Image source={education} className="h-full" resizeMode="contain" />
+			<Image
+				source={require('../../assets/images/education.webp')}
+				className="h-full"
+				resizeMode="contain"
+			/>
 		</View>
 	)
 }
