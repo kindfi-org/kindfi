5c774c26ad1cb6b5906968a43684fdb9b2953d0b
Meka-tech <65835906+Meka-tech@users.noreply.github.com>
2025-06-12 10:03:51 +0100

    [mobile-app] refactor: latam impact section types and constants (#549)

---
+++ apps/mobile/mobile-app/lib/utils.ts
@@ -0,0 +1,19 @@
+import { Dimensions } from 'react-native'
+
+export const getResponsiveLayout = () => {
+	const { width: screenWidth, height: screenHeight } = Dimensions.get('window')
+	const isTablet = screenWidth >= 768
+	const isLandscape = screenWidth > screenHeight
+
+	// Dynamic padding based on screen size
+	const CARD_PADDING = isTablet ? 32 : screenWidth < 375 ? 12 : 16
+	const CARD_WIDTH = screenWidth - CARD_PADDING * 2
+
+	return {
+		screenWidth,
+		CARD_PADDING,
+		CARD_WIDTH,
+		isTablet,
+		isLandscape,
+	}
+}
