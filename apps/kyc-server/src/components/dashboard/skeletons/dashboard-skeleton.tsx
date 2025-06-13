1d7d7927c7c1d1c929dbd13c3a2d4a8a1fcfa0b5
Derian <dmrodriguez2000@gmail.com>
2025-06-12 20:00:52 -0600

    refactor: move padding into child component for cleaner skeleton

--- apps/kyc-server/src/components/dashboard/skeletons/dashboard-skeleton.tsx
+++ apps/kyc-server/src/components/dashboard/skeletons/dashboard-skeleton.tsx
@@ -7,9 +7,7 @@ export function DashboardSkeleton() {
 		<div className="@container/main flex flex-1 flex-col gap-2">
 			<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
 				<MetricsGridSkeleton />
-				<div className="px-4 lg:px-6">
-					<SignupChartSkeleton />
-				</div>
+				<SignupChartSkeleton />
 				<KycTableSkeleton />
 			</div>
 		</div>
