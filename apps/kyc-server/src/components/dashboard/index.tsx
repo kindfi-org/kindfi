11c8775c41170d1743f75322d7453bbb448a96ed
Derian <dmrodriguez2000@gmail.com>
2025-06-12 19:55:00 -0600

    refactor: simplify conditional rendering to avoid extra DOM nesting

--- apps/kyc-server/src/components/dashboard/index.tsx
+++ apps/kyc-server/src/components/dashboard/index.tsx
@@ -22,21 +22,17 @@ export function Dashboard() {
 		return () => clearTimeout(timer)
 	}, [])
 
-	return (
-		<div className="flex flex-1 flex-col">
-			{isLoading ? (
-				<DashboardSkeleton />
-			) : (
-				<div className="@container/main flex flex-1 flex-col gap-2">
-					<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-						<MetricsGrid stats={mockKycStats} />
-						<div className="px-4 lg:px-6">
-							<SignupChart data={mockSignupChartData} />
-						</div>
-						<KycTable data={mockKycRecords} />
-					</div>
+	return isLoading ? (
+		<DashboardSkeleton />
+	) : (
+		<div className="@container/main flex flex-1 flex-col gap-2">
+			<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+				<MetricsGrid stats={mockKycStats} />
+				<div className="px-4 lg:px-6">
+					<SignupChart data={mockSignupChartData} />
 				</div>
-			)}
+				<KycTable data={mockKycRecords} />
+			</div>
 		</div>
 	)
 }
