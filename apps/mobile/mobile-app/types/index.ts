a3003e812d8a087cefa285d1c77e302aef9a2e9f
sheriffjimoh <sheriffjimoh88@gmail.com>
2025-06-28 08:20:03 +0100

    fix(mobile): item image type conflicting and added a getImageSource method in libtp handle images

--- apps/mobile/mobile-app/types/index.ts
+++ apps/mobile/mobile-app/types/index.ts
@@ -3,7 +3,6 @@ import type { ImageSourcePropType } from 'react-native'
 export interface Item {
 	id: string
 	title: string
-	imageUrl: string
 	category: string
 	image: string | ImageSourcePropType
 	text: string
