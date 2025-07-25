250144d52e4015c686aad132dacf0ed0bba15e63
Roberto 'Andler' Lucas <contact@andler.dev>
2025-07-24 23:09:12 -0600

    fix: app build

--- apps/web/lib/passkey/passkey.ts
+++ apps/web/lib/passkey/passkey.ts
@@ -145,7 +145,6 @@ export const verifyRegistration = async ({
 			 */
 			const newCredential: WebAuthnCredential = {
 				id: credential.id,
-				address: credential.address,
 				publicKey: credential.publicKey,
 				counter: credential.counter,
 				transports: registrationResponse.response.transports,
