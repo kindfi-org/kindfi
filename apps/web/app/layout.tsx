6275bff25e8e444286572d62b866b8d657ff2f5b
Benjtalkshow <chinedubenj@gmail.com>
2025-09-11 13:13:30 +0100

    fix: merge main

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,6 +6,9 @@ import { Header } from '~/components/shared/layout/header/header'
 import { Providers } from '~/components/shared/layout/providers'
 import './css/globals.css'
 import type { AppEnvInterface } from '@packages/lib/types'
+import * as Sentry from "@sentry/nextjs";
+
+
 
 const appConfig: AppEnvInterface = appEnvConfig('web')
 
@@ -23,6 +26,9 @@ export const metadata = {
 	openGraph: {
 		siteName: 'KindFi',
 		type: 'website',
+	},
+	other: {
+		...Sentry.getTraceData()
 	},
 }
 
--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,6 @@
-import { withSentryConfig } from '@sentry/nextjs';
 import { appEnvConfig } from '@packages/lib/config'
 import type { AppEnvInterface } from '@packages/lib/types'
+import { withSentryConfig } from '@sentry/nextjs'
 import type { NextConfig } from 'next'
 
 const appConfig: AppEnvInterface = appEnvConfig('web')
@@ -98,13 +98,14 @@ export default withSentryConfig(nextConfig, {
 	// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 	// Upload a larger set of source maps for prettier stack traces (increases build time)
+	authToken: process.env.SENTRY_AUTH_TOKEN,
 	widenClientFileUpload: true,
 
 	// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 	// This can increase your server load as well as your hosting bill.
 	// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
 	// side errors will fail.
-	tunnelRoute: "/monitoring",
+	tunnelRoute: '/monitoring',
 
 	// Automatically tree-shake Sentry logger statements to reduce bundle size
 	disableLogger: true,
@@ -114,4 +115,4 @@ export default withSentryConfig(nextConfig, {
 	// https://docs.sentry.io/product/crons/
 	// https://vercel.com/docs/cron-jobs
 	automaticVercelMonitors: true,
-});
+})
