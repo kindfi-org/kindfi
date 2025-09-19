0d7b7c3dbd25178aac6853935a75f0b01bbb634e
Benjtalkshow <chinedubenj@gmail.com>
2025-09-19 22:31:00 +0100

    fix: revert supabase types

--- apps/mobile/mobile-app/app/_layout.tsx
+++ apps/mobile/mobile-app/app/_layout.tsx
@@ -22,23 +22,23 @@ export {
 //   // Ensure that reloading on `/modal` keeps a back button present.
 //   initialRouteName: "gluestack",
 // };
-import * as Sentry from '@sentry/react-native';
+import * as Sentry from '@sentry/react-native'
 
 Sentry.init({
-  dsn: process.env.EXPO_SENTRY_DSN,
+	dsn: process.env.EXPO_SENTRY_DSN,
 
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
+	// Adds more context data to events (IP address, cookies, user, etc.)
+	// For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+	sendDefaultPii: true,
 
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration()],
+	// Configure Session Replay
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1,
+	integrations: [Sentry.mobileReplayIntegration()],
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
-});
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: __DEV__,
+})
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
@@ -70,7 +70,7 @@ export default Sentry.wrap(function RootLayout() {
 	// }
 
 	return <RootLayoutNav />
-});
+})
 
 function RootLayoutNav() {
 	const colorScheme = useColorScheme()
