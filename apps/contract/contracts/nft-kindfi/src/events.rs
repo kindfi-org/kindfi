168df5ab953e41bb8106df259d421380ad49f6dc
aguilar1x <aaguilar1x@gmail.com>
2026-01-23 17:00:25 -0600

    refactor: change in the use of events, since due to the sdk update they are now used with the contractevent and topic macro

--- apps/contract/contracts/nft-kindfi/src/events.rs
+++ apps/contract/contracts/nft-kindfi/src/events.rs
@@ -1,35 +1,30 @@
-use soroban_sdk::{contracttype, symbol_short, Address, Symbol};
+use soroban_sdk::{contractevent, Address};
 
 use crate::types::NFTMetadata;
 
-// Event topic symbols
-pub const NFT: Symbol = symbol_short!("NFT");
-pub const MINTED: Symbol = symbol_short!("MINTED");
-pub const BURNED: Symbol = symbol_short!("BURNED");
-pub const METADATA: Symbol = symbol_short!("METADATA");
-pub const UPDATED: Symbol = symbol_short!("UPDATED");
-
 /// Event data emitted when an NFT is minted with metadata.
-#[contracttype]
+#[contractevent]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct MintedEventData {
+    #[topic]
     pub token_id: u32,
     pub to: Address,
     pub metadata: NFTMetadata,
 }
 
-/// Event data emitted when an NFT is burned.
-#[contracttype]
+#[contractevent]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct BurnedEventData {
+    #[topic]
     pub token_id: u32,
     pub from: Address,
 }
 
 /// Event data emitted when NFT metadata is updated.
-#[contracttype]
+#[contractevent]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct MetadataUpdatedEventData {
+    #[topic]
     pub token_id: u32,
     pub metadata: NFTMetadata,
 }
